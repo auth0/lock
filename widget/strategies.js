8c3b7170d5c07e4ebf1272b063069a58601dbf7c
José F. Romaniello <jfromaniello@gmail.com>
2013-11-01 15:43:02 -0300

    extract strategies

---
+++ widget/strategies.js
@@ -0,0 +1,130 @@
+module.exports = {
+    'google-openid': {
+        css: 'google',
+        name: 'Google OpenId',
+        social: true
+    },
+    'google-apps': {
+        css: 'google',
+        name: 'Google Apps',
+        social: false
+    },
+    'google-oauth2': {
+        css: 'googleplus',
+        name: 'Google',
+        social: true
+    },
+    'facebook': {
+        css: 'facebook',
+        name: 'Facebook',
+        social: true
+    },
+    'windowslive': {
+        css: 'windows',
+        name: 'Microsoft Account',
+        social: true
+    },
+    'linkedin': {
+        css: 'linkedin',
+        name: 'LinkedIn',
+        social: true
+    },
+    'github': {
+        css: 'github',
+        name: 'GitHub',
+        social: true
+    },
+    'paypal': {
+        css: 'paypal',
+        name: 'PayPal',
+        social: true
+    },
+    'twitter': {
+        css: 'twitter',
+        name: 'Twitter',
+        social: true
+    },
+    'amazon': {
+        css: 'amazon',
+        name: 'Amazon',
+        social: true
+    },
+    'vkontakte': {
+        css: 'vk',
+        name: 'vKontakte',
+        social: true
+    },
+    'yandex': {
+        css: 'yandex',
+        name: 'Yandex',
+        social: true
+    },
+    'office365': {
+        css: 'office365',
+        name: 'Office365',
+        social: false
+    },
+    'waad': {
+        css: 'waad',
+        name: 'Windows Azure AD',
+        social: false
+    },
+    'adfs': {
+        css: 'windows',
+        name: 'ADFS',
+        social: false
+    },
+    'samlp': {
+        css: 'guest',
+        name: 'SAML',
+        social: false
+    },
+    'mscrm': {
+        css: 'guest',
+        name: 'Dynamics CRM',
+        social: false
+    },
+    'ad': {
+        css: 'windows',
+        name: 'AD / LDAP',
+        social: false
+    },
+    'custom': {
+        css: 'guest',
+        name: 'Custom Auth',
+        social: false
+    },
+    'auth0': {
+        css: 'guest',
+        name: 'Auth0',
+        social: false,
+        userAndPass: true
+    },
+    'auth0-adldap': {
+        css: 'guest',
+        name: 'AD/LDAP',
+        social: false,
+        userAndPass: true
+    },
+    'thirtysevensignals': {
+        css: 'thirtysevensignals',
+        name: '37 Signals',
+        social: true
+    },
+    'box': {
+        css: 'box',
+        name: 'Box',
+        social: true,
+        imageicon: true
+    },
+    'salesforce': {
+        css: 'salesforce',
+        name: 'Salesforce',
+        social: true
+    },
+    'fitbit': {
+        css: 'fitbit',
+        name: 'Fitbit',
+        social: true
+    }
+};
