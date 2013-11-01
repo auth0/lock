be51112a6ea0b7516f262a55c921c34a5821678b
José F. Romaniello <jfromaniello@gmail.com>
2013-11-01 18:03:29 -0300

    minor

--- build/auth0-widget.js
+++ build/auth0-widget.js
@@ -1035,7 +1035,7 @@ function decode(str) {
 }
 
 },{}],12:[function(require,module,exports){
-/*! version: 0.9.1 */
+/*! version: 0.9.3 */
 /*!
   * Reqwest! A general purpose XHR connection manager
   * (c) Dustin Diaz 2013
@@ -1050,7 +1050,7 @@ function decode(str) {
 
   var win = window
     , doc = document
-    , twoHundo = /^20\d$/
+    , twoHundo = /^(20\d|1223)$/
     , byTag = 'getElementsByTagName'
     , readyState = 'readyState'
     , contentType = 'Content-Type'
@@ -1572,7 +1572,7 @@ function decode(str) {
       // If traditional, encode the "old" way (the way 1.3.2 or older
       // did it), otherwise encode params recursively.
       for (prefix in o) {
-        buildParams(prefix, o[prefix], traditional, add)
+        if (o.hasOwnProperty(prefix)) buildParams(prefix, o[prefix], traditional, add)
       }
     }
 
@@ -6263,241 +6263,137 @@ Auth0Widget.prototype.show = function (signinOptions, callback) {
 
 module.exports = Auth0Widget;
 
-<<<<<<< HEAD
 },{"./html/main.html":25,"./strategies":28,"auth0-js":5,"bean":13,"bonzo":14,"qwery":16,"underscore":17,"xtend":19}],27:[function(require,module,exports){
-=======
-},{"./html/main.html":25,"./strategies":28,"auth0-js":2,"bean":13,"bonzo":14,"qwery":16,"underscore":17,"xtend":19}],27:[function(require,module,exports){
->>>>>>> minor
 /* Placeholders.js v3.0.0 */
 (function(t){"use strict";function e(t,e,r){return t.addEventListener?t.addEventListener(e,r,!1):t.attachEvent?t.attachEvent("on"+e,r):void 0}function r(t,e){var r,n;for(r=0,n=t.length;n>r;r++)if(t[r]===e)return!0;return!1}function n(t,e){var r;t.createTextRange?(r=t.createTextRange(),r.move("character",e),r.select()):t.selectionStart&&(t.focus(),t.setSelectionRange(e,e))}function a(t,e){try{return t.type=e,!0}catch(r){return!1}}t.Placeholders={Utils:{addEventListener:e,inArray:r,moveCaret:n,changeType:a}}})(this),function(t){"use strict";function e(){}function r(t,e){var r,n,a=!!e&&t.value!==e,u=t.value===t.getAttribute(V);return(a||u)&&"true"===t.getAttribute(D)?(t.removeAttribute(D),t.value=t.value.replace(t.getAttribute(V),""),t.className=t.className.replace(R,""),n=t.getAttribute(z),n&&(t.setAttribute("maxLength",n),t.removeAttribute(z)),r=t.getAttribute(I),r&&(t.type=r),!0):!1}function n(t){var e,r,n=t.getAttribute(V);return""===t.value&&n?(t.setAttribute(D,"true"),t.value=n,t.className+=" "+k,r=t.getAttribute(z),r||(t.setAttribute(z,t.maxLength),t.removeAttribute("maxLength")),e=t.getAttribute(I),e?t.type="text":"password"===t.type&&K.changeType(t,"text")&&t.setAttribute(I,"password"),!0):!1}function a(t,e){var r,n,a,u,i;if(t&&t.getAttribute(V))e(t);else for(r=t?t.getElementsByTagName("input"):p,n=t?t.getElementsByTagName("textarea"):b,i=0,u=r.length+n.length;u>i;i++)a=r.length>i?r[i]:n[i-r.length],e(a)}function u(t){a(t,r)}function i(t){a(t,n)}function l(t){return function(){m&&t.value===t.getAttribute(V)&&"true"===t.getAttribute(D)?K.moveCaret(t,0):r(t)}}function o(t){return function(){n(t)}}function c(t){return function(e){return f=t.value,"true"===t.getAttribute(D)&&f===t.getAttribute(V)&&K.inArray(C,e.keyCode)?(e.preventDefault&&e.preventDefault(),!1):void 0}}function s(t){return function(){r(t,f),""===t.value&&(t.blur(),K.moveCaret(t,0))}}function d(t){return function(){t===document.activeElement&&t.value===t.getAttribute(V)&&"true"===t.getAttribute(D)&&K.moveCaret(t,0)}}function g(t){return function(){u(t)}}function v(t){t.form&&(L=t.form,L.getAttribute(P)||(K.addEventListener(L,"submit",g(L)),L.setAttribute(P,"true"))),K.addEventListener(t,"focus",l(t)),K.addEventListener(t,"blur",o(t)),m&&(K.addEventListener(t,"keydown",c(t)),K.addEventListener(t,"keyup",s(t)),K.addEventListener(t,"click",d(t))),t.setAttribute(U,"true"),t.setAttribute(V,E),(m||t!==document.activeElement)&&n(t)}var p,b,m,h,f,A,y,E,x,L,T,N,S,w=["text","search","url","tel","email","password","number","textarea"],C=[27,33,34,35,36,37,38,39,40,8,46],B="#ccc",k="placeholdersjs",R=RegExp("(?:^|\\s)"+k+"(?!\\S)"),V="data-placeholder-value",D="data-placeholder-active",I="data-placeholder-type",P="data-placeholder-submit",U="data-placeholder-bound",j="data-placeholder-focus",q="data-placeholder-live",z="data-placeholder-maxlength",F=document.createElement("input"),G=document.getElementsByTagName("head")[0],H=document.documentElement,J=t.Placeholders,K=J.Utils;if(J.nativeSupport=void 0!==F.placeholder,!J.nativeSupport){for(p=document.getElementsByTagName("input"),b=document.getElementsByTagName("textarea"),m="false"===H.getAttribute(j),h="false"!==H.getAttribute(q),A=document.createElement("style"),A.type="text/css",y=document.createTextNode("."+k+" { color:"+B+"; }"),A.styleSheet?A.styleSheet.cssText=y.nodeValue:A.appendChild(y),G.insertBefore(A,G.firstChild),S=0,N=p.length+b.length;N>S;S++)T=p.length>S?p[S]:b[S-p.length],E=T.attributes.placeholder,E&&(E=E.nodeValue,E&&K.inArray(w,T.type)&&v(T));x=setInterval(function(){for(S=0,N=p.length+b.length;N>S;S++)T=p.length>S?p[S]:b[S-p.length],E=T.attributes.placeholder,E?(E=E.nodeValue,E&&K.inArray(w,T.type)&&(T.getAttribute(U)||v(T),(E!==T.getAttribute(V)||"password"===T.type&&!T.getAttribute(I))&&("password"===T.type&&!T.getAttribute(I)&&K.changeType(T,"text")&&T.setAttribute(I,"password"),T.value===T.getAttribute(V)&&(T.value=E),T.setAttribute(V,E)))):T.getAttribute(D)&&(r(T),T.removeAttribute(V));h||clearInterval(x)},100)}J.disable=J.nativeSupport?e:u,J.enable=J.nativeSupport?e:i}(this);
 },{}],28:[function(require,module,exports){
 module.exports = {
     'google-openid': {
         css: 'google',
-<<<<<<< HEAD
-        name: 'Google OpenId',
-=======
         title: 'Google OpenId',
->>>>>>> minor
         social: true
     },
     'google-apps': {
         css: 'google',
-<<<<<<< HEAD
-        name: 'Google Apps',
-=======
         title: 'Google Apps',
->>>>>>> minor
         social: false
     },
     'google-oauth2': {
         css: 'googleplus',
-<<<<<<< HEAD
-        name: 'Google',
-=======
         title: 'Google',
->>>>>>> minor
         social: true
     },
     'facebook': {
         css: 'facebook',
-<<<<<<< HEAD
-        name: 'Facebook',
-=======
         title: 'Facebook',
->>>>>>> minor
         social: true
     },
     'windowslive': {
         css: 'windows',
-<<<<<<< HEAD
-        name: 'Microsoft Account',
-=======
         title: 'Microsoft Account',
->>>>>>> minor
         social: true
     },
     'linkedin': {
         css: 'linkedin',
-<<<<<<< HEAD
-        name: 'LinkedIn',
-=======
         title: 'LinkedIn',
->>>>>>> minor
         social: true
     },
     'github': {
         css: 'github',
-<<<<<<< HEAD
-        name: 'GitHub',
-=======
         title: 'GitHub',
->>>>>>> minor
         social: true
     },
     'paypal': {
         css: 'paypal',
-<<<<<<< HEAD
-        name: 'PayPal',
-=======
         title: 'PayPal',
->>>>>>> minor
         social: true
     },
     'twitter': {
         css: 'twitter',
-<<<<<<< HEAD
-        name: 'Twitter',
-=======
         title: 'Twitter',
->>>>>>> minor
         social: true
     },
     'amazon': {
         css: 'amazon',
-<<<<<<< HEAD
-        name: 'Amazon',
-=======
         title: 'Amazon',
->>>>>>> minor
         social: true
     },
     'vkontakte': {
         css: 'vk',
-<<<<<<< HEAD
-        name: 'vKontakte',
-=======
         title: 'vKontakte',
->>>>>>> minor
         social: true
     },
     'yandex': {
         css: 'yandex',
-<<<<<<< HEAD
-        name: 'Yandex',
-=======
         title: 'Yandex',
->>>>>>> minor
         social: true
     },
     'office365': {
         css: 'office365',
-<<<<<<< HEAD
-        name: 'Office365',
-=======
         title: 'Office365',
->>>>>>> minor
         social: false
     },
     'waad': {
         css: 'waad',
-<<<<<<< HEAD
-        name: 'Windows Azure AD',
-=======
         title: 'Windows Azure AD',
->>>>>>> minor
         social: false
     },
     'adfs': {
         css: 'windows',
-<<<<<<< HEAD
-        name: 'ADFS',
-=======
         title: 'ADFS',
->>>>>>> minor
         social: false
     },
     'samlp': {
         css: 'guest',
-<<<<<<< HEAD
-        name: 'SAML',
-=======
         title: 'SAML',
->>>>>>> minor
         social: false
     },
     'mscrm': {
         css: 'guest',
-<<<<<<< HEAD
-        name: 'Dynamics CRM',
-=======
         title: 'Dynamics CRM',
->>>>>>> minor
         social: false
     },
     'ad': {
         css: 'windows',
-<<<<<<< HEAD
-        name: 'AD / LDAP',
-=======
         title: 'AD / LDAP',
->>>>>>> minor
         social: false
     },
     'custom': {
         css: 'guest',
-<<<<<<< HEAD
-        name: 'Custom Auth',
-=======
         title: 'Custom Auth',
->>>>>>> minor
         social: false
     },
     'auth0': {
         css: 'guest',
-<<<<<<< HEAD
-        name: 'Auth0',
-=======
         title: 'Auth0',
->>>>>>> minor
         social: false,
         userAndPass: true
     },
     'auth0-adldap': {
         css: 'guest',
-<<<<<<< HEAD
-        name: 'AD/LDAP',
-=======
         title: 'AD/LDAP',
->>>>>>> minor
         social: false,
         userAndPass: true
     },
     'thirtysevensignals': {
         css: 'thirtysevensignals',
-<<<<<<< HEAD
-        name: '37 Signals',
-=======
         title: '37 Signals',
->>>>>>> minor
         social: true
     },
     'box': {
         css: 'box',
-<<<<<<< HEAD
-        name: 'Box',
-=======
         title: 'Box',
->>>>>>> minor
         social: true,
         imageicon: true
     },
     'salesforce': {
         css: 'salesforce',
-<<<<<<< HEAD
-        name: 'Salesforce',
-=======
         title: 'Salesforce',
->>>>>>> minor
         social: true
     },
     'fitbit': {
         css: 'fitbit',
-<<<<<<< HEAD
-        name: 'Fitbit',
-=======
         title: 'Fitbit',
->>>>>>> minor
         social: true
     }
 };
--- example/auth0-widget.js
+++ example/auth0-widget.js
@@ -1035,7 +1035,7 @@ function decode(str) {
 }
 
 },{}],12:[function(require,module,exports){
-/*! version: 0.9.1 */
+/*! version: 0.9.3 */
 /*!
   * Reqwest! A general purpose XHR connection manager
   * (c) Dustin Diaz 2013
@@ -1050,7 +1050,7 @@ function decode(str) {
 
   var win = window
     , doc = document
-    , twoHundo = /^20\d$/
+    , twoHundo = /^(20\d|1223)$/
     , byTag = 'getElementsByTagName'
     , readyState = 'readyState'
     , contentType = 'Content-Type'
@@ -1572,7 +1572,7 @@ function decode(str) {
       // If traditional, encode the "old" way (the way 1.3.2 or older
       // did it), otherwise encode params recursively.
       for (prefix in o) {
-        buildParams(prefix, o[prefix], traditional, add)
+        if (o.hasOwnProperty(prefix)) buildParams(prefix, o[prefix], traditional, add)
       }
     }
 
@@ -6263,8 +6263,139 @@ Auth0Widget.prototype.show = function (signinOptions, callback) {
 
 module.exports = Auth0Widget;
 
-},{"./html/main.html":25,"auth0-js":2,"bean":13,"bonzo":14,"qwery":16,"underscore":17,"xtend":19}],27:[function(require,module,exports){
+},{"./html/main.html":25,"./strategies":28,"auth0-js":5,"bean":13,"bonzo":14,"qwery":16,"underscore":17,"xtend":19}],27:[function(require,module,exports){
 /* Placeholders.js v3.0.0 */
 (function(t){"use strict";function e(t,e,r){return t.addEventListener?t.addEventListener(e,r,!1):t.attachEvent?t.attachEvent("on"+e,r):void 0}function r(t,e){var r,n;for(r=0,n=t.length;n>r;r++)if(t[r]===e)return!0;return!1}function n(t,e){var r;t.createTextRange?(r=t.createTextRange(),r.move("character",e),r.select()):t.selectionStart&&(t.focus(),t.setSelectionRange(e,e))}function a(t,e){try{return t.type=e,!0}catch(r){return!1}}t.Placeholders={Utils:{addEventListener:e,inArray:r,moveCaret:n,changeType:a}}})(this),function(t){"use strict";function e(){}function r(t,e){var r,n,a=!!e&&t.value!==e,u=t.value===t.getAttribute(V);return(a||u)&&"true"===t.getAttribute(D)?(t.removeAttribute(D),t.value=t.value.replace(t.getAttribute(V),""),t.className=t.className.replace(R,""),n=t.getAttribute(z),n&&(t.setAttribute("maxLength",n),t.removeAttribute(z)),r=t.getAttribute(I),r&&(t.type=r),!0):!1}function n(t){var e,r,n=t.getAttribute(V);return""===t.value&&n?(t.setAttribute(D,"true"),t.value=n,t.className+=" "+k,r=t.getAttribute(z),r||(t.setAttribute(z,t.maxLength),t.removeAttribute("maxLength")),e=t.getAttribute(I),e?t.type="text":"password"===t.type&&K.changeType(t,"text")&&t.setAttribute(I,"password"),!0):!1}function a(t,e){var r,n,a,u,i;if(t&&t.getAttribute(V))e(t);else for(r=t?t.getElementsByTagName("input"):p,n=t?t.getElementsByTagName("textarea"):b,i=0,u=r.length+n.length;u>i;i++)a=r.length>i?r[i]:n[i-r.length],e(a)}function u(t){a(t,r)}function i(t){a(t,n)}function l(t){return function(){m&&t.value===t.getAttribute(V)&&"true"===t.getAttribute(D)?K.moveCaret(t,0):r(t)}}function o(t){return function(){n(t)}}function c(t){return function(e){return f=t.value,"true"===t.getAttribute(D)&&f===t.getAttribute(V)&&K.inArray(C,e.keyCode)?(e.preventDefault&&e.preventDefault(),!1):void 0}}function s(t){return function(){r(t,f),""===t.value&&(t.blur(),K.moveCaret(t,0))}}function d(t){return function(){t===document.activeElement&&t.value===t.getAttribute(V)&&"true"===t.getAttribute(D)&&K.moveCaret(t,0)}}function g(t){return function(){u(t)}}function v(t){t.form&&(L=t.form,L.getAttribute(P)||(K.addEventListener(L,"submit",g(L)),L.setAttribute(P,"true"))),K.addEventListener(t,"focus",l(t)),K.addEventListener(t,"blur",o(t)),m&&(K.addEventListener(t,"keydown",c(t)),K.addEventListener(t,"keyup",s(t)),K.addEventListener(t,"click",d(t))),t.setAttribute(U,"true"),t.setAttribute(V,E),(m||t!==document.activeElement)&&n(t)}var p,b,m,h,f,A,y,E,x,L,T,N,S,w=["text","search","url","tel","email","password","number","textarea"],C=[27,33,34,35,36,37,38,39,40,8,46],B="#ccc",k="placeholdersjs",R=RegExp("(?:^|\\s)"+k+"(?!\\S)"),V="data-placeholder-value",D="data-placeholder-active",I="data-placeholder-type",P="data-placeholder-submit",U="data-placeholder-bound",j="data-placeholder-focus",q="data-placeholder-live",z="data-placeholder-maxlength",F=document.createElement("input"),G=document.getElementsByTagName("head")[0],H=document.documentElement,J=t.Placeholders,K=J.Utils;if(J.nativeSupport=void 0!==F.placeholder,!J.nativeSupport){for(p=document.getElementsByTagName("input"),b=document.getElementsByTagName("textarea"),m="false"===H.getAttribute(j),h="false"!==H.getAttribute(q),A=document.createElement("style"),A.type="text/css",y=document.createTextNode("."+k+" { color:"+B+"; }"),A.styleSheet?A.styleSheet.cssText=y.nodeValue:A.appendChild(y),G.insertBefore(A,G.firstChild),S=0,N=p.length+b.length;N>S;S++)T=p.length>S?p[S]:b[S-p.length],E=T.attributes.placeholder,E&&(E=E.nodeValue,E&&K.inArray(w,T.type)&&v(T));x=setInterval(function(){for(S=0,N=p.length+b.length;N>S;S++)T=p.length>S?p[S]:b[S-p.length],E=T.attributes.placeholder,E?(E=E.nodeValue,E&&K.inArray(w,T.type)&&(T.getAttribute(U)||v(T),(E!==T.getAttribute(V)||"password"===T.type&&!T.getAttribute(I))&&("password"===T.type&&!T.getAttribute(I)&&K.changeType(T,"text")&&T.setAttribute(I,"password"),T.value===T.getAttribute(V)&&(T.value=E),T.setAttribute(V,E)))):T.getAttribute(D)&&(r(T),T.removeAttribute(V));h||clearInterval(x)},100)}J.disable=J.nativeSupport?e:u,J.enable=J.nativeSupport?e:i}(this);
+},{}],28:[function(require,module,exports){
+module.exports = {
+    'google-openid': {
+        css: 'google',
+        title: 'Google OpenId',
+        social: true
+    },
+    'google-apps': {
+        css: 'google',
+        title: 'Google Apps',
+        social: false
+    },
+    'google-oauth2': {
+        css: 'googleplus',
+        title: 'Google',
+        social: true
+    },
+    'facebook': {
+        css: 'facebook',
+        title: 'Facebook',
+        social: true
+    },
+    'windowslive': {
+        css: 'windows',
+        title: 'Microsoft Account',
+        social: true
+    },
+    'linkedin': {
+        css: 'linkedin',
+        title: 'LinkedIn',
+        social: true
+    },
+    'github': {
+        css: 'github',
+        title: 'GitHub',
+        social: true
+    },
+    'paypal': {
+        css: 'paypal',
+        title: 'PayPal',
+        social: true
+    },
+    'twitter': {
+        css: 'twitter',
+        title: 'Twitter',
+        social: true
+    },
+    'amazon': {
+        css: 'amazon',
+        title: 'Amazon',
+        social: true
+    },
+    'vkontakte': {
+        css: 'vk',
+        title: 'vKontakte',
+        social: true
+    },
+    'yandex': {
+        css: 'yandex',
+        title: 'Yandex',
+        social: true
+    },
+    'office365': {
+        css: 'office365',
+        title: 'Office365',
+        social: false
+    },
+    'waad': {
+        css: 'waad',
+        title: 'Windows Azure AD',
+        social: false
+    },
+    'adfs': {
+        css: 'windows',
+        title: 'ADFS',
+        social: false
+    },
+    'samlp': {
+        css: 'guest',
+        title: 'SAML',
+        social: false
+    },
+    'mscrm': {
+        css: 'guest',
+        title: 'Dynamics CRM',
+        social: false
+    },
+    'ad': {
+        css: 'windows',
+        title: 'AD / LDAP',
+        social: false
+    },
+    'custom': {
+        css: 'guest',
+        title: 'Custom Auth',
+        social: false
+    },
+    'auth0': {
+        css: 'guest',
+        title: 'Auth0',
+        social: false,
+        userAndPass: true
+    },
+    'auth0-adldap': {
+        css: 'guest',
+        title: 'AD/LDAP',
+        social: false,
+        userAndPass: true
+    },
+    'thirtysevensignals': {
+        css: 'thirtysevensignals',
+        title: '37 Signals',
+        social: true
+    },
+    'box': {
+        css: 'box',
+        title: 'Box',
+        social: true,
+        imageicon: true
+    },
+    'salesforce': {
+        css: 'salesforce',
+        title: 'Salesforce',
+        social: true
+    },
+    'fitbit': {
+        css: 'fitbit',
+        title: 'Fitbit',
+        social: true
+    }
+};
 },{}]},{},[27,24])
 ;
