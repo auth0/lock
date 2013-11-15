2a2c9b7527b4952d744781511557b6ca267a9ee5
José F. Romaniello <jfromaniello@gmail.com>
2013-11-15 16:49:34 -0300

    improve signin with db connections on mobile

---
+++ widget/js/clean_login_onfocus.js
@@ -0,0 +1,23 @@
+var bean = require('bean');
+
+module.exports.hook = function ($) {
+  var timeoutId;
+
+  function on_blur() {
+    timeoutId = setTimeout(function () {
+      $('.a0-collapse-social').show();
+      $('.a0-header').show();
+    }, 5);
+  }
+
+  function on_focus () {
+    clearTimeout(timeoutId);
+    $('.a0-collapse-social').hide();
+    $('.a0-header').hide();
+  }
+
+  bean.on($('#a0-signin_easy_email')[0], 'blur', on_blur);
+  bean.on($('#a0-signin_easy_email')[0], 'focus', on_focus);
+  bean.on($('#a0-signin_easy_password')[0], 'blur', on_blur);
+  bean.on($('#a0-signin_easy_password')[0], 'focus', on_focus);
+};
