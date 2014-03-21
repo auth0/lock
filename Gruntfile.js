23c3224e21cf929b43ce13bbd4f07a9a4364ee09
José F. Romaniello <jfromaniello@gmail.com>
2014-03-21 16:41:19 -0300

    disable browserstack ie10 test

--- Gruntfile.js
+++ Gruntfile.js
@@ -128,7 +128,7 @@ module.exports = function (grunt) {
         stderr: true
       },
       'test-desktop': {
-        cmd: 'testem ci -l bs_chrome,bs_firefox,bs_ie_9,bs_ie_10', //disable ,bs_ie_8 is not working
+        cmd: 'testem ci -l bs_chrome,bs_firefox', //disable ,bs_ie_9,bs_ie_10,bs_ie_8 Browserstack is not working
         stdout: true,
         stderr: true
       },
