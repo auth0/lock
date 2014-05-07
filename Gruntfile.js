6ced1a46f0493b771f8d1f5a39de70e0b051f5b7
Cristian Douce <cristian@gravityonmars.com>
2014-05-07 17:33:29 -0300

    Update example builds

--- Gruntfile.js
+++ Gruntfile.js
@@ -43,6 +43,9 @@ module.exports = function (grunt) {
     },
     jade: {
       example: {
+        options: {
+          pretty: true
+        },
         files: {
           "example/build/index.html": "example/index.jade"
         }
