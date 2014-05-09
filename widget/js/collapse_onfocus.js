fb22b3e4acbbb2e394e0f5683ae5d48652a9bd3f
Ricardo Rauch <rickyrauch@gmail.com>
2014-05-09 17:42:05 -0300

    fixes css

--- widget/js/collapse_onfocus.js
+++ widget/js/collapse_onfocus.js
@@ -12,19 +12,19 @@ module.exports.hook = function (inputs, collapsible) {
 
   function on_focus () {
     clearTimeout(timeoutId);
-    collapsible.hide();
-    $('.a0-header').hide();
+    // collapsible.hide();
+    // $('.a0-header').hide();
   }
 
-  inputs
-    .a0_off('blur.collapse_onfocus')
-    .a0_on('blur.collapse_onfocus', on_blur)
-    .a0_off('focus.collapse_onfocus')
-    .a0_on('focus.collapse_onfocus', on_focus)
-    .a0_off('touchstart.ts_collapse_onfocus')
-    .a0_on('touchstart.ts_collapse_onfocus', function (e) {
-      setTimeout(function () {
-        e.target.focus();
-      }, 500);
-    });
+  // inputs
+  //   .a0_off('blur.collapse_onfocus')
+  //   .a0_on('blur.collapse_onfocus', on_blur)
+  //   .a0_off('focus.collapse_onfocus')
+  //   .a0_on('focus.collapse_onfocus', on_focus)
+  //   .a0_off('touchstart.ts_collapse_onfocus')
+  //   .a0_on('touchstart.ts_collapse_onfocus', function (e) {
+  //     setTimeout(function () {
+  //       e.target.focus();
+  //     }, 500);
+  //   });
 };
