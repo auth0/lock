8fcd1be03bf225e4af38e37c19e818d1af10ba04
Gabriel Andretta <gabriel.andretta@gmail.com>
2015-07-20 18:05:33 -0300

    Use always the singular entity name in store

--- src/store/index.js
+++ src/store/index.js
@@ -1,7 +1,7 @@
 import atom from '../atom/index';
 import Immutable, { Map } from 'immutable';
 
-const store = atom(Immutable.fromJS({clients: {}, gravatars: {}, locks: {}}));
+const store = atom(Immutable.fromJS({client: {}, gravatar: {}, lock: {}}));
 
 function set(state) {
   store.reset(state);
@@ -32,43 +32,43 @@ function getEntity(coll, id) {
 }
 
 function updateLocks(f) {
-  updateCollection("locks", f);
+  updateCollection("lock", f);
 }
 
 export function updateLocksWithClient(clientID, f) {
-  updateFilteredCollection("locks", l => l.get("clientID") === clientID, f);
+  updateFilteredCollection("lock", l => l.get("clientID") === clientID, f);
 }
 
 export function updateLock(id, f, ...args) {
-  updateEntity("locks", id, f, ...args);
+  updateEntity("lock", id, f, ...args);
 }
 
 export function setLock(lock) {
-  setEntity("locks", lock.get("id"), lock);
+  setEntity("lock", lock.get("id"), lock);
 }
 
 export function getLock(id) {
-  getEntity("locks", id);
+  getEntity("lock", id);
 }
 
 export function setClient(client) {
-  setEntity("clients", client.get("id"), client);
+  setEntity("client", client.get("id"), client);
 }
 
 function getClient(id) {
-  return getEntity("clients", id);
+  return getEntity("client", id);
 }
 
 export function getGravatar(email) {
-  return getEntity("gravatars", email);
+  return getEntity("gravatar", email);
 }
 
 export function updateGravatar(email, f) {
-  updateEntity("gravatars", email, f);
+  updateEntity("gravatar", email, f);
 }
 
 function deriveUIState(state) {
-  return state.get("locks").map(lock => {
+  return state.get("lock").map(lock => {
     const gravatar = getGravatar(lock.get("email")) || new Map({});
     lock = lock.set("gravatar", gravatar.get("loaded") ? gravatar : null);
     return lock.set("client", getClient(lock.get("clientID")));
