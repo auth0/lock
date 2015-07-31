30ca79e2e33975b7d45da2909fc4873af6c713ac
Gabriel Andretta <gabriel.andretta@gmail.com>
2015-07-31 16:27:32 -0300

    Customize and send client info headers, closes #19

--- src/lock/web_api.js
+++ src/lock/web_api.js
@@ -1,6 +1,9 @@
 import Auth0 from 'auth0-js';
 import * as StringUtils from '../utils/string_utils';
 
+Auth0.clientInfo.name +=  " (LockPasswordless)";
+Auth0.clientInfo.version += " (LockPasswordless)";
+
 class Auth0WebAPI {
   constructor() {
     this.clients = {};
@@ -8,7 +11,11 @@ class Auth0WebAPI {
 
   setupClient(lockID, clientID, domain) {
     // TODO: reuse clients
-    this.clients[lockID] = new Auth0({clientID: clientID, domain: domain});
+    this.clients[lockID] = new Auth0({
+      clientID: clientID,
+      domain: domain,
+      sendSDKClientInfo: true
+    });
   }
 
   signIn(lockID, options, cb) {
