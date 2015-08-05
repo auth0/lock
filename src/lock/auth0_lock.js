eb7601d5520e02ba3e7e2cf4d2189e3a255299a7
Gabriel Andretta <gabriel.andretta@gmail.com>
2015-08-05 08:15:50 -0300

    Informative messages for errors opening the Lock

--- src/lock/auth0_lock.js
+++ src/lock/auth0_lock.js
@@ -35,44 +35,6 @@ export default class Auth0Lock {
     setupLock(this.id, clientID, domain);
   }
 
-  // open(mode, optionsOrCallback, callback) {
-  //   // Argument validation
-  //   if (typeof mode != "string") {
-  //     throw new Error("A `mode` string must be provided as first argument.");
-  //   }
-  //
-  //   let options;
-  //
-  //   if (arguments.length == 2) {
-  //     if (typeof optionsOrCallback == "object") {
-  //       options = optionsOrCallback;
-  //     } else if (typeof optionsOrCallback == "function") {
-  //       callback = optionsOrCallback
-  //     } else {
-  //       throw new Error("An `option` object or a `callback` function must be provided as a second argument.");
-  //     }
-  //   } else {
-  //     options = optionsOrCallback;
-  //     if (typeof options != "object") {
-  //       throw new Error("An `option` object must be provided as a second argument.");
-  //     }
-  //     if (typeof callback != "function") {
-  //       throw new Error("A `callback` function must be provided as a third argument.");
-  //     }
-  //   }
-  //
-  //   // Dispatch according to the given mode
-  //   const modeOpen = Auth0Lock.modeOpen(mode);
-  //
-  //   if (typeof modeOpen != "function") {
-  //     throw new Error("Unknown `mode` " + mode + ".");
-  //   }
-  //
-  //   options = options || {};
-  //   options.signInCallback = callback;
-  //   modeOpen(this.id, options);
-  // }
-
   close() {
     closeLock(this.id);
   }
--- src/passwordless-email/mode_spec.js
+++ src/passwordless-email/mode_spec.js
@@ -3,14 +3,47 @@ import render from './render';
 
 const NAME = "passwordless-email";
 
-function magiclink(id, options, callback) {
+function openFunctionArgsResolver(fnName, args) {
+  const defaultOptions = {};
+  const defaultCallback = () => {};
+
+  if (args.length == 0) {
+    return [defaultOptions, defaultCallback];
+  }
+
+  if (args.length == 1) {
+    if (typeof args[0] == "object") {
+      return [args[0], defaultCallback];
+    } else if (typeof args[0] == "function") {
+      return [defaultOptions, args[0]];
+    } else {
+      throw new Error("When `" + fnName + "` is called with one argument, it must be an `options` object or a `callback` function.");
+    }
+  }
+
+  if (args.length == 2) {
+    if (typeof args[0] != "object") {
+      throw new Error("When `" + fnName + "` is called with two arguments, an `options` object must be provided as the first argument.");
+    }
+    if (typeof args[1] != "function") {
+      throw new Error("When `" + fnName + "` is called with two arguments, a `callback` function must be provided as the second argument.");
+    }
+    return args;
+  }
+
+  throw new Error("`" + fnName + "` must be called with two arguments at most.");
+}
+
+function magiclink(id, ...args) {
+  const [options, callback] = openFunctionArgsResolver("magiclink", args);
   options.signInCallback = callback;
   openLock(id, NAME, options);
 }
 
-function emailcode(id, options, callback) {
-  options.modeOptions = {send: "code"};
+function emailcode(id, ...args) {
+  const [options, callback] = openFunctionArgsResolver("emailcode", args);
   options.signInCallback = callback;
+  options.modeOptions = {send: "code"};
   openLock(id, NAME, options);
 }
 
