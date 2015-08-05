711b01795387969d5e1a93895b6efca055eed123
Benjamin Flores <beneliflo@gmail.com>
2015-08-05 14:44:04 -0300

    sms initial

--- js/site.js
+++ js/site.js
@@ -15,15 +15,34 @@ $(function() {
     $('.auth0-lock-form').toggleClass("auth0-lock-hide");
   });
 
+  $('.auth0-lock-input-location').on('click', function() {
+
+    console.log('select country');
+
+    $('.auth0-lock').addClass('auth0-lock-mode-country');
+
+    return false;
+  });
+
+  $('.auth0-lock-list-code').on('click', function() {
+
+    console.log('country selected');
+
+    $('.auth0-lock').removeClass('auth0-lock-mode-country');
+
+    $('.auth0-lock').addClass('auth0-lock-mode-initial');
+
+  });
+
   $('.auth0-lock-submit').click(function(){
 
-    if ($('.auth0-lock-input-email').val() === '') {
+    if ($('.auth0-lock-input-number').val() === '') {
 
 
       $('.auth0-lock-input-block-email').addClass('auth0-lock-error');
       $('.auth0-lock-input-block-email').addClass('animated pulse');
 
-    }else if ($('.auth0-lock-input-email').val() === "rickyrauch@gmail.com") {
+    }else if ($('.auth0-lock-input-number').val() === "11-3344-5566") {
 
       $('.auth0-lock').addClass('auth0-lock-mode-loading');
 
@@ -70,7 +89,7 @@ $(function() {
     return false;
   });
 
-  $( ".auth0-lock-input-email" ).focusin(function() {
+  $( ".auth0-lock-input-number" ).focusin(function() {
 
     if ($('.auth0-lock-input-block-email').hasClass('auth0-lock-error') ) {
 
@@ -79,19 +98,19 @@ $(function() {
     }
   });
 
-  $(".auth0-lock-input-email").keyup(function() {
-    if (this.value == "rickyrauch@gmail.com") {
-      $(".auth0-lock-header-logo").hide();
-      $('.auth0-lock-header-bg-inner').css('background-image','url(img/avatar.png)').addClass("auth0-lock-no-grayscale");
-      $(".auth0-lock-header-avatar").removeClass("auth0-lock-hide").addClass('animated fadeInDown');
-      $('.auth0-lock-firstname').removeClass("auth0-lock-hide");
-      $('.auth0-lock-name').hide();
-    }
-    else {
-    // $(".auth0-lock-header-logo").show();
-    // $(".auth0-lock-header-avatar").hide();
-    // $('.auth0-lock-firstname').hide();
-    // $('.auth0-lock-name').show();
-    }
-  });
+  // $(".auth0-lock-input-number").keyup(function() {
+  //   if (this.value == "11-3344-5566") {
+  //     $(".auth0-lock-header-logo").hide();
+  //     $('.auth0-lock-header-bg-inner').css('background-image','url(img/avatar.png)').addClass("auth0-lock-no-grayscale");
+  //     $(".auth0-lock-header-avatar").removeClass("auth0-lock-hide").addClass('animated fadeInDown');
+  //     $('.auth0-lock-firstname').removeClass("auth0-lock-hide");
+  //     $('.auth0-lock-name').hide();
+  //   }
+  //   else {
+  //     $(".auth0-lock-header-logo").show();
+  //     $(".auth0-lock-header-avatar").hide();
+  //     $('.auth0-lock-firstname').hide();
+  //     $('.auth0-lock-name').show();
+  //   }
+  // });
 });
