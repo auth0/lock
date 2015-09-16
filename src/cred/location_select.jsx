22107f50f1d4bd29a3b1fa44c0c2e4bd0628e812
Gabriel Andretta <gabriel.andretta@gmail.com>
2015-09-16 12:08:22 -0300

    Handle location highlight with JS

--- src/cred/location_select.jsx
+++ src/cred/location_select.jsx
@@ -6,11 +6,12 @@ import * as su from '../utils/string_utils';
 export default class LocationSelect extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {search: ""};
+    this.state = {search: "", highlighted: null};
   }
 
   render() {
     const { autoFocus, selectHandler } = this.props;
+    const { highlighted } = this.state;
 
     const countryCodes = cc.countryCodes.filter(x => {
       return su.matches(this.state.search, cc.country(x));
@@ -29,7 +30,9 @@ export default class LocationSelect extends React.Component {
           </div>
         </div>
         <LocationList countryCodes={countryCodes}
-            selectHandler={selectHandler} />
+          highlighted={highlighted}
+          highlightHandler={::this.handleHighlight}
+          selectHandler={selectHandler} />
       </div>
     );
   }
@@ -37,33 +40,62 @@ export default class LocationSelect extends React.Component {
   handleSearchChange(e) {
     this.setState({search: e.target.value});
   }
+
+  handleHighlight(location) {
+    this.setState({highlighted: location});
+  }
 }
 
 class LocationList extends React.Component {
   render() {
-    const { countryCodes, selectHandler } = this.props;
+    const { countryCodes, highlighted, highlightHandler, selectHandler } = this.props;
 
     const items = countryCodes.map(x => {
       const key = cc.locationString(x).replace(/ /g, '-');
 
       return (
-        <LocationListItem location={x} key={key} onClick={selectHandler} />
+        <LocationListItem location={x}
+          key={key}
+          highlighted={highlighted === x}
+          highlightHandler={highlightHandler}
+          selectHandler={selectHandler} />
       );
     });
 
-    return <div className="auth0-lock-list-code"><ul>{items}</ul></div>;
+    return (
+      <div className="auth0-lock-list-code" onMouseLeave={::this.handleMouseLeave}>
+        <ul>{items}</ul>
+      </div>
+    );
+  }
+
+  handleMouseLeave() {
+    this.props.highlightHandler(null);
   }
 }
 
 class LocationListItem extends React.Component {
   render() {
-    const { location } = this.props;
-    return <li onClick={::this.handleClick}>{cc.locationString(location)}</li>;
+    const { highlighted, location } = this.props;
+    const className = highlighted ? "auth0-lock-list-code-highlighted" : "";
+
+    return (
+      <li className={className}
+        onClick={::this.handleClick}
+        onMouseEnter={::this.handleMouseEnter}>
+          {cc.locationString(location)}
+      </li>
+    );
   }
 
   handleClick(e) {
     e.preventDefault();
-    const { location, onClick } = this.props;
-    onClick(location);
+    const { location, selectHandler } = this.props;
+    selectHandler(location);
+  }
+
+  handleMouseEnter(e) {
+    const { location, highlightHandler } = this.props;
+    highlightHandler(location);
   }
 }
