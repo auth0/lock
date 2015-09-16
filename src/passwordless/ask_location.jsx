e0be8158a8d5d7ce59aebacc49a7aef44baac57c
Gabriel Andretta <gabriel.andretta@gmail.com>
2015-09-16 11:41:05 -0300

    Autofocus location select unless in small screen

--- src/passwordless/ask_location.jsx
+++ src/passwordless/ask_location.jsx
@@ -2,12 +2,13 @@ import React from 'react';
 import LocationSelect from '../cred/location_select';
 import { changePhoneLocation } from './actions';
 import * as l from '../lock/index';
+import { isSmallScreen } from '../utils/media_utils';
 
 export default class AskLocation extends React.Component {
   render() {
     return (
       <LocationSelect selectHandler={::this.locationSelectHandler}
-        autoFocus={l.ui.focusInput(this.props.lock)} />
+        autoFocus={!isSmallScreen()} />
     );
   }
 
