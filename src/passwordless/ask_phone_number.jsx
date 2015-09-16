b72cba237c51c8421bdb9e66ec394bbc1fba5523
Gabriel Andretta <gabriel.andretta@gmail.com>
2015-09-16 10:38:12 -0300

    Remove unneeded SMS heading

--- src/passwordless/ask_phone_number.jsx
+++ src/passwordless/ask_phone_number.jsx
@@ -18,7 +18,6 @@ export default class AskPhoneNumber extends React.Component {
       <CredPane lock={lock} auxiliaryPane={auxiliaryPane} className="auth0-lock-intro" showTerms={true} ref="cred">
         <div className="auth0-lock-passwordless auth0-lock-mode">
           <div className="auth0-lock-form auth0-lock-passwordless">
-            <h2>SMS</h2>
             <p>Please enter your phone number.</p>
             <PhoneNumberInput value={c.phoneNumber(lock)}
               isValid={!c.visiblyInvalidPhoneNumber(lock)}
