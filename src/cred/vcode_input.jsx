87652a1f9ca8ae62f33cf2a64e080fd96e193abd
Gabriel Andretta <gabriel.andretta@gmail.com>
2015-09-18 14:38:51 -0300

    Highlight on focus VcodeInput

--- src/cred/vcode_input.jsx
+++ src/cred/vcode_input.jsx
@@ -3,19 +3,35 @@ import InputWrap from './input_wrap';
 import Icon from '../icon/icon';
 
 export default class VcodeInput extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
   render() {
     const { isValid, ...props } = this.props;
+    const { focused } = this.state;
 
     return (
-      <InputWrap name="vcode" isValid={isValid} icon={<Icon name="vcode" />}>
+      <InputWrap name="vcode" isValid={isValid} icon={<Icon name="vcode" />}  focused={focused}>
         <input type="text"
           className="auth0-lock-input auth0-lock-input-code"
           placeholder="Your code"
           autocomplete="off"
           autocapitalize="off"
+          onFocus={::this.handleFocus}
+          onBlur={::this.handleBlur}
           {...props} />
       </InputWrap>
     );
+  }
+
+  handleFocus() {
+    this.setState({focused: true});
+  }
+
+  handleBlur() {
+    this.setState({focused: false});
   }
 }
 
