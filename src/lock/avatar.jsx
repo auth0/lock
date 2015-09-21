d2d2e3ae2a9dfa4871fbc9235964637c5979727e
Benjamin Flores <beneliflo@gmail.com>
2015-09-21 12:40:53 -0300

    Change fadein mobile

--- src/lock/avatar.jsx
+++ src/lock/avatar.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 export default class Avatar extends React.Component {
   render() {
     const { imageUrl } = this.props;
-    return <img src={imageUrl} className="auth0-lock-header-avatar animated fadeInDown" />;
+    return <img src={imageUrl} className="auth0-lock-header-avatar" />;
   }
 }
 
