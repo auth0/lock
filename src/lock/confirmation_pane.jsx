2f3ffcaccc0d05f32aa075356e01b526051c77ea
Benjamin Flores <beneliflo@gmail.com>
2015-09-29 18:16:49 -0300

    Fix little issues with IE

--- src/lock/confirmation_pane.jsx
+++ src/lock/confirmation_pane.jsx
@@ -3,7 +3,7 @@ import IconButton from '../icon/button';
 
 class CheckmarkIcon extends React.Component {
   render() {
-    const svgTag = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 52 52" class="checkmark"> <circle cx="26" cy="26" r="25" fill="none" class="checkmark__circle"></circle> <path fill="none" d="M14.1 27.2l7.1 7.2 16.7-16.8" class="checkmark__check"></path> </svg>';
+    const svgTag = '<svg width="56px" height="56px" viewBox="0 0 52 52" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" class="checkmark"> <circle cx="26" cy="26" r="25" fill="none" class="checkmark__circle"></circle> <path fill="none" d="M14.1 27.2l7.1 7.2 16.7-16.8" class="checkmark__check"></path> </svg>';
     return <span dangerouslySetInnerHTML={{__html: svgTag}} />;
   }
 }
