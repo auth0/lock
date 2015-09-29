cb0157f468d4fa5371f2e85c73c33d1ab08bf32c
mcabral <cabral.martin@gmail.com>
2015-09-29 11:56:03 -0300

    Minor style updates

--- support/playground/index.js
+++ support/playground/index.js
@@ -1,39 +1,41 @@
-$('#code-modal').on('show.bs.modal', function (e) {
-  $('#code-modal').css('display','block');
-});
-
-$('#show-code').on('click', function (ev) {
-  $('#lock-code code').text(getLockInitializationCode());
-  hljs.highlightBlock($('#lock-code code').get(0));
-});
-
-$('#show-lock').on('click', function (ev) {
-  ev.preventDefault();
-
-  var method = $('[name="method"]').val();
-  var clientID = $('[name="clientID"]').val();
-  var domain = $('[name="domain"]').val();
-  var lock = new Auth0LockPasswordless(clientID, domain);
-
-  var options = getOptions();
-
-  // Exectue Lock with options
-  lock[method](options, function (err, profile, id_token, access_token, state, refresh_token) {
-    var outputContainer = $('#output code');
-
-    if (err) {
-      outputContainer.text('Lock encountered an error:\n' + err.description);
-      return;
-    }
-
-    if (method === 'magiclink') {
-      outputContainer.text('Email sent to ' + profile);
-    } else {
-      outputContainer.text('{\n  access_token: "' + access_token + '",\n  id_token: "' + id_token + '"\n}');
-      hljs.highlightBlock(outputContainer.get(0));
-    }
+function bindEvents () {
+  $('#code-modal').on('show.bs.modal', function (e) {
+    $('#code-modal').css('display','block');
   });
-});
+
+  $('#show-code').on('click', function (ev) {
+    $('#lock-code code').text(getLockInitializationCode());
+    hljs.highlightBlock($('#lock-code code').get(0));
+  });
+
+  $('#show-lock').on('click', function (ev) {
+    ev.preventDefault();
+
+    var method = $('[name="method"]').val();
+    var clientID = $('[name="clientID"]').val();
+    var domain = $('[name="domain"]').val();
+    var lock = new Auth0LockPasswordless(clientID, domain);
+
+    var options = getOptions();
+
+    // Exectue Lock with options
+    lock[method](options, function (err, profile, id_token, access_token, state, refresh_token) {
+      var outputContainer = $('#output code');
+
+      if (err) {
+        outputContainer.text('Lock encountered an error:\n' + err.description);
+        return;
+      }
+
+      if (method === 'magiclink') {
+        outputContainer.text('Email sent to ' + profile);
+      } else {
+        outputContainer.text('{\n  access_token: "' + access_token + '",\n  id_token: "' + id_token + '"\n}');
+        hljs.highlightBlock(outputContainer.get(0));
+      }
+    });
+  });
+}
 
 function getLockInitializationCode () {
   var template = $('#lock-init-template').text();
@@ -70,3 +72,5 @@ function getOptions () {
 
   return options;
 }
+
+bindEvents();
