c49af3ec06395d9684e64c8d8f9477db1c8818fe
Gabriel Andretta <gabriel.andretta@gmail.com>
2015-09-29 23:28:12 -0300

    Attempt to bypass blur filter on IE

--- src/header/background.jsx
+++ src/header/background.jsx
@@ -1,22 +1,43 @@
 import React from 'react';
 import { isSmallScreen } from '../utils/media_utils';
 
+const cssBlurSupport = (function() {
+  // Check stolen from Modernizr, see https://github.com/Modernizr/Modernizr/blob/29eab707f7a2fb261c8a9c538370e97eb1f86e25/feature-detects/css/filters.js
+  const el = global.document.createElement('div');
+  el.style.cssText = "filter: blur(2px); -webkit-filter: blur(2px)";
+  return !!el.style.length && (global.document.documentMode === undefined || global.document.documentMode > 9);
+})();
+
 export default class Background extends React.Component {
   render() {
     const { backgroundColor, imageUrl, grayScale } = this.props;
-    let bgInnerAttrs = {
+
+    const props = {
+      className: "auth0-lock-header-bg"
+    };
+
+    if (cssBlurSupport) {
+      props.className += " auth0-lock-blur-support";
+    }
+
+    const blurProps = {
       className: 'auth0-lock-header-bg-blur',
       style: {backgroundImage: `url('${imageUrl}')`}
     };
 
     if (grayScale) {
-      bgInnerAttrs.className += ' auth0-lock-no-grayscale';
+      blurProps.className += ' auth0-lock-no-grayscale';
+    }
+
+    const solidProps = {
+      className: "auth0-lock-header-bg-solid",
+      style: {backgroundColor: backgroundColor}
     }
 
     return (
-      <div className="auth0-lock-header-bg">
-        <div {...bgInnerAttrs} />
-        <div className="auth0-lock-header-bg-solid" style={{backgroundColor: backgroundColor}} />
+      <div {...props}>
+        <div {...blurProps} />
+        <div {...solidProps} />
       </div>
     );
   }
