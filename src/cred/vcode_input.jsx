fd397323c46c27db98ccab3ed207427fd8647fe2
Gabriel Andretta <gnandretta@users.noreply.github.com>
2015-10-06 20:59:32 -0300

    Use type="tel" for verification code input

--- src/cred/vcode_input.jsx
+++ src/cred/vcode_input.jsx
@@ -25,7 +25,7 @@ export default class VcodeInput extends React.Component {
     return (
       <InputWrap name="vcode" isValid={isValid} icon={<Icon name="vcode" />}  focused={focused}>
         <input ref="input"
-          type="number"
+          type="tel"
           name="vcode"
           className="auth0-lock-input auth0-lock-input-code"
           autoComplete="off"
