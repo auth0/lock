704647be8e7015a0b800d9d24c8f83cd9ed0a755
Gabriel Andretta <gabriel.andretta@gmail.com>
2015-10-20 12:26:22 -0300

    Move social/magiclink mode along with the others

---
+++ src/modes/social-or-magiclink/spec.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import Lock from '../../lock/lock';
+import AskSocialNetworkOrEmail from '../../cred/or/ask_social_network_or_email';
+import { openLock } from '../../lock/actions';
+import { openFunctionArgsResolver } from '../../lock/mode';
+import { close } from '../../social/actions';
+import { requestPasswordlessEmail } from '../../passwordless/actions';
+import * as l from '../../lock/index';
+import * as mp from '../../passwordless/index';
+
+const NAME = "socialOrMagiclink";
+
+function open(id, ...args) {
+  const [options, callback] = openFunctionArgsResolver(NAME, args);
+  options.signInCallback = callback;
+  // TODO: review cred storage
+  options.modeOptions = {dictName: NAME, storageKey: NAME};
+  return openLock(id, NAME, options);
+}
+
+function askEmailSubmitHandler(lock) {
+  requestPasswordlessEmail(l.id(lock));
+}
+
+function render(lock) {
+  const props = {
+    children: <AskSocialNetworkOrEmail key="social-network-or-email" lock={lock} />,
+    closeHandler: close,
+    escHandler: close,
+    isDone: mp.passwordlessStarted(lock) || l.signedIn(lock),
+    lock: lock,
+    submitHandler: !mp.passwordlessStarted(lock) && askEmailSubmitHandler
+  };
+
+  return <Lock {...props} />;
+}
+
+export default {
+  name: NAME,
+  methods: {
+    close: close,
+    open: {
+      socialOrMagiclink: open
+    }
+  },
+  renderFn: render
+};
