1d89684053cc5b7481cd968c441c2e6fe1a1bb54
Gabriel Andretta <gabriel.andretta@gmail.com>
2015-11-04 10:46:17 -0300

    Remove no longer used stuff from AskEmail

--- src/cred/email/ask_email.jsx
+++ src/cred/email/ask_email.jsx
@@ -5,18 +5,8 @@ import EmailPane from './email_pane';
 
 export default class AskEmail extends Screen {
 
-  constructor(lock, submitHandler, renderAuxilaryPane) {
+  constructor(lock) {
     super("email", lock);
-    this._submitHandler = submitHandler;
-    this._renderAuxiliaryPane = renderAuxilaryPane;
-  }
-
-  submitHandler() {
-    return this._submitHandler;
-  }
-
-  renderAuxiliaryPane() {
-    return this._renderAuxiliaryPane;
   }
 
   render({lock}) {
