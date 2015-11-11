36344f2d47989ad09c98739e917615bbe5357c5f
Gabriel Andretta <gabriel.andretta@gmail.com>
2015-11-11 17:26:36 -0300

    Add PasswordInput component

---
+++ src/cred/password/password_input.jsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import InputWrap from '../input_wrap';
+import Icon from '../../icon/icon';
+
+export default class PasswordInput extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    const { isValid, onChange, ...props } = this.props;
+    const { focused } = this.state;
+
+    return (
+      // TODO: use a proper icon
+      <InputWrap name="email" isValid={isValid} icon={<Icon name="vcode" />} focused={focused}>
+        <input
+          ref="input"
+          type="password"
+          name="password"
+          className="auth0-lock-input"
+          placeholder="enter password"
+          autoComplete="off"
+          autoCapitalize="off"
+          onChange={::this.handleOnChange}
+          onFocus={::this.handleFocus}
+          onBlur={::this.handleBlur}
+          {...props}
+        />
+      </InputWrap>
+    );
+  }
+
+  handleOnChange(e) {
+    if (this.props.onChange) {
+      this.props.onChange(e);
+    }
+  }
+
+  handleFocus() {
+    this.setState({focused: true});
+  }
+
+  handleBlur() {
+    this.setState({focused: false});
+  }
+}
+
+// TODO: specify propTypes
