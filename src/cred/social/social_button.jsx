c2fc2dfbf3af007de73ff10ef28ffe96852dbfee
Gabriel Andretta <gabriel.andretta@gmail.com>
2015-11-13 11:21:24 -0300

    Include social icons

--- src/cred/social/social_button.jsx
+++ src/cred/social/social_button.jsx
@@ -6,14 +6,6 @@ import { useBigButtons } from '../../social/index';
 export default class SocialButton extends React.Component {
   render() {
     const { connection, disabled, lock, tabIndex } = this.props;
-    const colors = {
-      "facebook": "#4761b0",
-      "github": "#666666",
-      "google-oauth2": "#df4a32",
-      "twitter": "#46c0fb",
-      "windowslive": "#0078d5"
-    };
-    const color = colors[connection.strategy] || colors["github"];
 
     let className = "auth0-lock-social-button";
     if (useBigButtons(lock)) className += " auth0-lock-social-big-button";
@@ -21,9 +13,9 @@ export default class SocialButton extends React.Component {
     return (
       <button
         className={className}
+        data-provider={connection.strategy}
         disabled={disabled}
         onClick={::this.handleClick}
-        style={{backgroundColor: color}}
         tabIndex={tabIndex}
         type="button"
       >
