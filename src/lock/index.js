6b9b79cfa694a5ee85d7009d2da8283a5c6b68c5
Gabriel Andretta <gabriel.andretta@gmail.com>
2015-12-29 14:08:48 -0300

    Add fns to register and retrieve connections

--- src/lock/index.js
+++ src/lock/index.js
@@ -247,3 +247,21 @@ export function warn(x, str) {
     console.warn(str);
   }
 }
+
+export function registerConnection(m, type, strategy, name) {
+  return m.update(
+    "connections",
+    List(),
+    xs => xs.push(Map({type, strategy, name}))
+  );
+}
+
+export function registerConnections(m, type, connections) {
+  return connections.reduce((r, x) => (
+    registerConnection(r, type, x.strategy, x.name)
+  ), m);
+}
+
+export function getConnections(m) {
+  return m.get("connections", List());
+}
