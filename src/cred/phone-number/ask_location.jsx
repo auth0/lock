1097892d4b51155dc4759c5fd15e6c0436503637
Gabriel Andretta <gabriel.andretta@gmail.com>
2016-01-05 18:07:02 -0300

    Update code style in AskLocation

--- src/cred/phone-number/ask_location.jsx
+++ src/cred/phone-number/ask_location.jsx
@@ -5,14 +5,6 @@ import { initialLocationSearchStr, selectingLocation } from './index';
 import * as l from '../../lock/index';
 
 export default class AskLocation extends React.Component {
-  render() {
-    return (
-      <LocationSelect selectHandler={::this.handleSelect}
-        cancelHandler={::this.handleCancel}
-        initialLocationSearchStr={this.props.initialLocationSearchStr}
-        locationFilterInputPlaceholder={this.t(["locationFilterInputPlaceholder"], {__textOnly: true})} />
-    );
-  }
 
   handleCancel() {
     cancelSelectPhoneLocation(l.id(this.props.lock));
@@ -24,6 +16,17 @@ export default class AskLocation extends React.Component {
 
   t(keyPath, params) {
     return l.ui.t(this.props.lock, ["location"].concat(keyPath), params);
+  }
+
+  render() {
+    return (
+      <LocationSelect
+        cancelHandler={::this.handleCancel}
+        initialLocationSearchStr={this.props.initialLocationSearchStr}
+        locationFilterInputPlaceholder={this.t(["locationFilterInputPlaceholder"], {__textOnly: true})}
+        selectHandler={::this.handleSelect}
+      />
+    );
   }
 
 }
