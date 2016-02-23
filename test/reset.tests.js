10dd7c4a445c5fde4cb7ce1803f900b89b6354cf
Gabriel Andretta <gabriel.andretta@gmail.com>
2016-02-23 17:48:13 -0300

    Fix tests

--- test/reset.tests.js
+++ test/reset.tests.js
@@ -27,93 +27,176 @@ describe('reset', function () {
     this.widget.hide(done);
   });
 
-  describe('Reset widget', function() {
+  describe('Reset widget (legacy)', function() {
     it('should show the loading pane on submit', function (done) {
-    var widget = this.widget;
-    var readyLoading = false;
+      var widget = this.widget;
+      var readyLoading = false;
 
-    this.client.changePassword = function(options, callback) {
-      callback(null, "success message");
-    };
-
-    widget
-    .once('reset ready', function () {
-      $('#a0-reset_easy_email').val('ohmy@mandatory.com');
-      $('#a0-reset_easy_password').val('123');
-      $('#a0-reset_easy_repeat_password').val('123');
+      this.client.changePassword = function(options, callback) {
+        callback(null, "success message");
+      };
 
       widget
-      .once('loading ready', function () {
-        readyLoading = true;
-        expect($('#a0-lock h1').html()).to.be(widget.options.i18n.t('reset:title'));
-      });
+      .once('reset ready', function () {
+        $('#a0-reset_easy_email').val('ohmy@mandatory.com');
+        $('#a0-reset_easy_password').val('123');
+        $('#a0-reset_easy_repeat_password').val('123');
 
-      bean.fire($('.a0-reset form')[0], 'submit');
-    })
-    .showReset({}, onsuccess);
+        widget
+        .once('loading ready', function () {
+          readyLoading = true;
+          expect($('#a0-lock h1').html()).to.be(widget.options.i18n.t('reset:title'));
+        })
+        .once('signin ready', function() {
+          expect(readyLoading).to.be(true);
+          done();
+        });
 
-    function onsuccess() {
-      expect(readyLoading).to.be(true);
-      done();
-    }
+        bean.fire($('.a0-reset form')[0], 'submit');
+      })
+      .showReset({useNewReset: false});
+    });
+
+    it('should not invoke callback function when reset is successfull', function(done) {
+      var widget = this.widget;
+
+      // mock success from reset password
+      this.client.changePassword = function(options, callback) {
+        callback(null, "success message");
+      }
+
+      widget
+      .once('reset ready', function () {
+        $('#a0-reset_easy_email').val('ohmy@mandatory.com');
+        $('#a0-reset_easy_password').val('123');
+        $('#a0-reset_easy_repeat_password').val('123');
+
+        bean.fire($('.a0-reset form')[0], 'submit');
+      })
+      .once('reset success', function() {
+        done();
+      })
+      .showReset({useNewReset: false}, cb);
+
+      function cb() {
+        expect(false).to.be.ok(); // callback should not be invoked
+        done();
+      }
+    });
+
+    it('should not invoke callback function with error when reset errors', function(done) {
+      var widget = this.widget;
+
+      // mock error from reset password
+      this.client.changePassword = function(options, callback) {
+        callback(new Error('Email does not exist'))
+      }
+
+      widget
+      .once('reset ready', function () {
+        $('#a0-reset_easy_email').val('unregistered@email.com');
+        $('#a0-reset_easy_password').val('123');
+        $('#a0-reset_easy_repeat_password').val('123');
+
+        bean.fire($('.a0-reset form')[0], 'submit');
+      })
+      .once('reset error', function() {
+        done();
+      })
+      .showReset({useNewReset: false}, cb);
+
+      function cb() {
+        expect(false).to.be.ok(); // callback should not be invoked
+        done();
+      }
+    });
   });
 
-  it('should invoke callback function when reset is successfull', function(done) {
-    var widget = this.widget;
+  describe('Reset widget (new)', function() {
+    it('should show the loading pane on submit', function (done) {
+      var widget = this.widget;
+      var readyLoading = false;
 
-    // mock success from reset password
-    this.client.changePassword = function(options, callback) {
-      callback(null, "success message");
-    }
+      this.client.changePassword = function(options, callback) {
+        callback(null, "success message");
+      };
 
-    widget
-    .once('reset ready', function () {
-      $('#a0-reset_easy_email').val('ohmy@mandatory.com');
-      $('#a0-reset_easy_password').val('123');
-      $('#a0-reset_easy_repeat_password').val('123');
+      widget
+      .once('reset ready', function () {
+        $('#a0-reset_easy_email').val('ohmy@mandatory.com');
 
-      bean.fire($('.a0-reset form')[0], 'submit');
-    })
-    .showReset({}, onsuccess);
+        widget
+        .once('loading ready', function () {
+          readyLoading = true;
+          expect($('#a0-lock h1').html()).to.be(widget.options.i18n.t('newReset:title'));
+        })
+        .once('signin ready', function() {
+          expect(readyLoading).to.be(true);
+          done();
+        });
 
-    function onsuccess(err, message) {
-      expect(err).to.equal(null);
-      expect(message).to.not.be(undefined);
-      done();
-    }
+        bean.fire($('.a0-reset form')[0], 'submit');
+      })
+      .showReset();
+    });
+
+    it('should not invoke callback function when reset is successfull', function(done) {
+      var widget = this.widget;
+
+      // mock success from reset password
+      this.client.changePassword = function(options, callback) {
+        callback(null, "success message");
+      }
+
+      widget
+      .once('reset ready', function () {
+        $('#a0-reset_easy_email').val('ohmy@mandatory.com');
+
+        bean.fire($('.a0-reset form')[0], 'submit');
+      })
+      .once('reset success', function() {
+        done();
+      })
+      .showReset(cb);
+
+      function cb() {
+        expect(false).to.be.ok(); // callback should not be invoked
+        done();
+      }
+    });
+
+    it('should not invoke callback function with error when reset errors', function(done) {
+      var widget = this.widget;
+
+      // mock error from reset password
+      this.client.changePassword = function(options, callback) {
+        callback(new Error('Email does not exist'))
+      }
+
+      widget
+      .once('reset ready', function () {
+        $('#a0-reset_easy_email').val('unregistered@email.com');
+
+        bean.fire($('.a0-reset form')[0], 'submit');
+      })
+      .once('reset error', function() {
+        done();
+      })
+      .showReset(cb);
+
+      function cb() {
+        expect(false).to.be.ok(); // callback should not be invoked
+        done();
+      }
+    });
   });
 
-  it('should invoke callback function with error when reset errors', function(done) {
-    var widget = this.widget;
-
-    // mock error from reset password
-    this.client.changePassword = function(options, callback) {
-      callback(new Error('Email does not exist'))
-    }
-
-    widget
-    .once('reset ready', function () {
-      $('#a0-reset_easy_email').val('unregistered@email.com');
-      $('#a0-reset_easy_password').val('123');
-      $('#a0-reset_easy_repeat_password').val('123');
-
-      bean.fire($('.a0-reset form')[0], 'submit');
-    })
-    .showReset({}, onerror);
-
-    function onerror(err, message) {
-      expect(err).to.not.be(null);
-      expect(message).to.be(undefined);
-      done();
-    }
-  });
-  });
-
-  describe('when requires_username is enabled', function() {
+  describe('when requires_username is enabled (for legacy only)', function() {
 
     beforeEach(function() {
       // Mock `_isUsernameRequired` so it asumes database has enabled
       // requires_username on it's configuration
+      this.options.useNewReset = false;
       this.options._isUsernameRequired = function() { return true; };
     });
 
@@ -219,7 +302,11 @@ describe('reset', function () {
     });
   });
 
-  describe('Events', function() {
+  describe('Events (legacy)', function() {
+    beforeEach(function() {
+      this.options.useNewReset = false;
+    });
+
     it('should fire up an submit event on submit', function(done) {
         var auth0 = this.widget;
         var email = 'pepo@example.com';
@@ -363,6 +450,83 @@ describe('reset', function () {
         $('#a0-reset_easy_email').val(username);
         $('#a0-reset_easy_password').val(password);
         $('#a0-reset_easy_repeat_password').val(password);
+
+        bean.fire($('.a0-reset form')[0], 'submit');
+      })
+      .showReset(this.options);
+    });
+
+  });
+
+  describe('Events (new)', function() {
+    it('should fire up an submit event on submit', function(done) {
+      var auth0 = this.widget;
+      var email = 'pepo@example.com';
+
+      this.client.changePassword = function() {};
+
+      auth0
+      .once('reset submit', function(opts) {
+        expect(opts).to.not.be(undefined);
+        done();
+      })
+      .once('reset ready', function() {
+        $('#a0-reset_easy_email').val(email);
+
+        bean.fire($('.a0-reset form')[0], 'submit');
+      })
+      .showReset(this.options);
+    });
+
+
+    it('should fire up an error event on email invalid', function(done) {
+      var auth0 = this.widget;
+      var email = 'pepo!&&*example.com';
+
+      auth0
+      .once('reset error', function(err) {
+        expect(err.message).to.be('email invalid');
+        done();
+      })
+      .once('reset ready', function() {
+        $('#a0-reset_easy_email').val(email);
+
+        bean.fire($('.a0-reset form')[0], 'submit');
+      })
+      .showReset(this.options);
+    });
+
+    it('should fire up an error event on username empty', function (done) {
+      var auth0 = this.widget;
+      var email = 'pepo@example.com';
+
+      this.options._isUsernameRequired = function() { return true; };
+
+      auth0
+      .once('reset error', function(err) {
+        expect(err.message).to.be('username empty');
+        done();
+      })
+      .once('reset ready', function() {
+
+        bean.fire($('.a0-reset form')[0], 'submit');
+      })
+      .showReset(this.options);
+    });
+
+    it('should fire up an error event on username invalid', function (done) {
+      var auth0 = this.widget;
+      var username = '1.1.1.1';
+
+      this.options._isUsernameRequired = function() { return true; };
+
+      auth0
+      .once('reset error', function(err) {
+        expect(err.message).to.be('username invalid');
+        done();
+      })
+      .once('reset ready', function() {
+        $('#a0-reset_easy_email').val(username);
 
         bean.fire($('.a0-reset form')[0], 'submit');
       })
