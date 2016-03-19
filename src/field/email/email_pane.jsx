b6bed0e33cb7c7e7e72e42aa6d2ec0f344dddce1
Gabriel Andretta <gabriel.andretta@gmail.com>
2016-03-19 16:05:33 -0300

    Remove gravatar dep from EmailInput, move to ui

--- src/field/email/email_pane.jsx
+++ src/field/email/email_pane.jsx
@@ -1,13 +1,25 @@
 import React from 'react';
-import EmailInput from './email_input';
+import EmailInput from '../../ui/input/email_input';
 import * as c from '../index';
 import { swap, updateEntity } from '../../store/index';
 import * as l from '../../lock/index';
 import { setEmail } from '../email';
+import { debouncedRequestGravatar, requestGravatar } from '../../gravatar/actions';
 
 export default class EmailPane extends React.Component {
 
+  componentDidMount() {
+    const { lock } = this.props;
+    if (l.ui.gravatar(lock) && c.email(lock)) {
+      requestGravatar(c.email(lock));
+    }
+  }
+
   handleChange(e) {
+    if (l.ui.gravatar(this.props.lock)) {
+      debouncedRequestGravatar(e.target.value);
+    }
+
     swap(updateEntity, "lock", l.id(this.props.lock), setEmail, e.target.value);
   }
 
