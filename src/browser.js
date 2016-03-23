84c1ad262db25451311962105594f8f4e34bf387
Gabriel Andretta <gabriel.andretta@gmail.com>
2016-03-23 20:23:31 -0300

    Fix heroku build

--- src/browser.js
+++ src/browser.js
@@ -8,15 +8,15 @@
  */
 
 import Auth0Lock from './classic';
-// import Auth0LockPasswordless from './passwordless';
+import Auth0LockPasswordless from './passwordless';
 
 global.window.Auth0Lock = Auth0Lock;
 // global.window.Auth0LockPasswordless = Auth0LockPasswordless;
 //use amd or just throught to window object.
 if (typeof global.window.define == 'function' && global.window.define.amd) {
   global.window.define('auth0Lock', function () { return Auth0Lock; });
-//  global.window.define('auth0LockPasswordless', function () { return Auth0LockPasswordless; });
+  global.window.define('auth0LockPasswordless', function () { return Auth0LockPasswordless; });
 } else if (global.window) {
   global.window.Auth0Lock = Auth0Lock;
-//  global.window.Auth0LockPasswordless = Auth0LockPasswordless;
+  global.window.Auth0LockPasswordless = Auth0LockPasswordless;
 }
