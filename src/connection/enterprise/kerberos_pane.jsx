047f1ac0eb37e8454997672ad2d25923cfb49b40
Gabriel Andretta <gabriel.andretta@gmail.com>
2016-04-05 14:49:28 -0300

    Delegate most KerberosPane's work to QuickAuthPane

--- src/connection/enterprise/kerberos_pane.jsx
+++ src/connection/enterprise/kerberos_pane.jsx
@@ -1,52 +1,32 @@
 import React from 'react';
 import { skipSSOLogin } from '../../lock/sso/actions';
 import * as l from '../../lock/index';
-
-const KerberosButton = () => (
-  <button
-    className="auth0-lock-social-button auth0-lock-social-big-button"
-    data-provider="windows"
-    disabled={false}
-    onClick={e => alert("not implemented")}
-    type="button"
-  >
-    <div className="auth0-lock-social-button-icon" />
-    <div className="auth0-lock-social-button-text">
-      Windows Authentication
-    </div>
-  </button>
-);
+import QuickAuthPane from '../../ui/pane/quick_auth_pane';
 
 export default class KerberosPane extends React.Component {
 
   handleClick(e) {
     e.preventDefault();
+    // TODO: instead of calling skipSSOLogin we should have something
+    // generic for this quick logins.
     skipSSOLogin(l.id(this.props.lock));
   }
 
   render() {
     const { header, lock, skipLastLoginLabel } = this.props;
 
+    // TODO: implement click handler.
+    // TODO: provide translation for button label.
+
     return (
-      <div className="auth0-lock-last-login-pane">
-        {header}
-
-        <KerberosButton />
-
-        <p className="auth0-lock-alternative">
-          <a
-            className="auth0-lock-alternative-link"
-            href="#"
-            onClick={::this.handleClick}
-          >
-            {skipLastLoginLabel}
-          </a>
-        </p>
-
-        <div className="auth0-loading-container">
-          <div className="auth0-loading" />
-        </div>
-      </div>
+      <QuickAuthPane
+        alternativeLabel={skipLastLoginLabel}
+        alternativeClickHandler={::this.handleClick}
+        buttonLabel="Windows Authentication"
+        buttonClickHandler={e => alert("not implemented")}
+        header={header}
+        strategy="windows"
+      />
     );
   }
 
