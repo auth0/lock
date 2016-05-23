c1a0f007ae13f65958d5c44d3ec259d98088335b
Gabriel Andretta <gabriel.andretta@gmail.com>
2016-05-23 16:50:19 -0300

    Abstract fetching stuff from the CDN

--- src/core/client/settings.js
+++ src/core/client/settings.js
@@ -1,36 +1,10 @@
-import Auth0 from 'auth0-js';
-import Cache from '../../utils/cache';
+import { load } from '../../utils/cdn_utils';
 
-const cache = new Cache((...args) => addClientScriptTag(...args));
-
-export function fetchClientSettings(clientID, assetsUrl, cb = () => {}) {
-  cache.get(clientID, assetsUrl, cb);
-}
-
-const cbs = {};
-
-function addClientScriptTag(clientID, assetsUrl, cb) {
-  cbs[clientID] = cb;
-  const script = global.document.createElement('script');
-  script.src = `${assetsUrl}/client/${clientID}.js?t${+new Date()}`;
-  global.document.getElementsByTagName('head')[0].appendChild(script);
-
-  const handleError = () => cb({});
-
-  const timeoutID = setTimeout(handleError, 5000);
-
-  script.addEventListener('load', () => clearTimeout(timeoutID));
-
-  script.addEventListener('error', () => {
-    clearTimeout(timeoutID);
-    handleError();
+export function fetchClientSettings(clientID, assetsUrl, cb) {
+  load({
+    method: "setClient",
+    url: `${assetsUrl}/client/${clientID}.js?t${+new Date()}`,
+    check: o => o && o.id === clientID,
+    cb: cb
   });
-
 }
-
-global.window.Auth0 = Auth0;
-
-global.window.Auth0.setClient = function(settings) {
-  cbs[settings.id](null, settings);
-  delete cbs[settings.id];
-};
---
+++ src/utils/cdn_utils.js
@@ -0,0 +1,53 @@
+if (!global.Auth0) {
+  global.Auth0 = {};
+}
+
+const cbs = {};
+
+export function load(attrs) {
+  const { cb, check, method, url } = attrs;
+
+  if (!cbs[method]) {
+    cbs[method] = [];
+    global.Auth0[method] = function(...args) {
+      cbs[method] = cbs[method].filter(x => {
+        if (x.check(...args)) {
+          setTimeout(() => x.cb(null, ...args), 0);
+          return false;
+        } else {
+          return true;
+        }
+      });
+    }
+  }
+
+  cbs[method].push({cb: cb, check: check, url: url});
+
+  const count = cbs[method].reduce((r, x) => r + (x.url === url ? 1 : 0), 0);
+
+  if (count > 1) return;
+
+  const script = global.document.createElement('script');
+  script.src = url;
+  global.document.getElementsByTagName('head')[0].appendChild(script);
+
+  const handleError = (url) => {
+    cbs[method] = cbs[method].filter(x => {
+      if (x.url === url) {
+        setTimeout(() => x.cb({}), 0);
+        return false;
+      } else {
+        return true;
+      }
+    });
+  }
+
+  const timeoutID = setTimeout(() => handleError(url), 5000);
+
+  script.addEventListener('load', () => clearTimeout(timeoutID));
+
+  script.addEventListener('error', () => {
+    clearTimeout(timeoutID);
+    handleError(url);
+  });
+}
