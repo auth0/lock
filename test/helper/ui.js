9bd1c67f1a31df94d182ceb4f70e1007a1765642
Gabriel Andretta <gabriel.andretta@gmail.com>
2016-05-24 10:45:24 -0300

    Increase delays in test for travis

--- test/helper/ui.js
+++ test/helper/ui.js
@@ -78,8 +78,8 @@ export const displayLock = (name, opts = {}, done = () => {}) => {
   }
 
   const lock = new Auth0Lock("cid", "domain", opts);
-  setTimeout(() => lock.show(), 70);
-  setTimeout(done, 75);
+  setTimeout(() => lock.show(), 100);
+  setTimeout(done, 105);
   return lock;
 };
 
