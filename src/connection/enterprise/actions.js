9ce9a18970a614e745171339d6e15fcb2431bad8
Gabriel Andretta <gabriel.andretta@gmail.com>
2016-05-26 10:17:39 -0300

    Delegate actual login to core in logInSSO

--- src/connection/enterprise/actions.js
+++ src/connection/enterprise/actions.js
@@ -1,16 +1,13 @@
 import { getEntity, read, swap, updateEntity } from '../../store/index';
 import {
   enterpriseConnection,
-  findADConnectionWithoutDomain,
   isHRDActive,
   matchConnection,
   toggleHRD
 } from '../enterprise';
-import * as l from '../../core/index';
 import * as c from '../../field/index';
 import { setUsername } from '../../field/username';
 import { emailLocalPart } from '../../field/email';
-import webApi from '../../core/web_api';
 import { logIn as coreLogIn } from '../../core/actions';
 
 export function startHRD(id) {
@@ -51,53 +48,10 @@ function logInActiveFlow(id) {
 }
 
 function logInSSO(id, connection) {
-  swap(updateEntity, "lock", id, lock => {
-    if (c.isFieldValid(lock, "email")) {
-      return l.setSubmitting(lock, true);
-    } else {
-      lock = c.setFieldShowInvalid(lock, "email", !c.isFieldValid(lock, "email"));
-      return lock;
-    }
+  const m = read(getEntity, "lock", id);
+
+  coreLogIn(id, ["email"], {
+    connection: connection.get("name"),
+    login_hint: c.getFieldValue(m, "email")
   });
-
-  const lock = read(getEntity, "lock", id);
-
-  if (l.submitting(lock)) {
-    const options = {
-      connection: connection.get("name"),
-      login_hint: c.email(lock)
-    };
-
-    webApi.logIn(
-      id,
-      options,
-      (error, ...args) => {
-        if (error) {
-          setTimeout(() => logInError(id, error), 250);
-        } else {
-          logInSuccess(id, ...args);
-        }
-      }
-    );
-  }
-}
-
-function logInSuccess(id, ...args) {
-  const lock = read(getEntity, "lock", id);
-  const autoclose = l.ui.autoclose(lock);
-
-  if (!autoclose) {
-    swap(updateEntity, "lock", id, lock => l.setSignedIn(l.setSubmitting(lock, false), true));
-    l.invokeLogInCallback(lock, null, ...args);
-  } else {
-    closeLock(id, false, lock => l.invokeLogInCallback(lock, null, ...args));
-  }
-}
-
-function logInError(id, error) {
-  const lock = read(getEntity, "lock", id);
-  const errorMessage = l.loginErrorMessage(lock, error);
-
-  swap(updateEntity, "lock", id, l.setSubmitting, false, errorMessage);
-  l.invokeLogInCallback(lock, error);
 }
