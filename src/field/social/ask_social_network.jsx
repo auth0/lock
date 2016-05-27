2f0a52da9815964099b89d155161e5c6a9b77ef0
Gabriel Andretta <gabriel.andretta@gmail.com>
2016-05-27 14:50:27 -0300

    Use i18n prop instead of t in AskSocialNetwork

--- src/field/social/ask_social_network.jsx
+++ src/field/social/ask_social_network.jsx
@@ -3,13 +3,13 @@ import Screen from '../../core/screen';
 import SocialButtonsPane from './social_buttons_pane';
 import { renderSignedInConfirmation } from '../../core/signed_in_confirmation';
 
-const Component = ({i18n, model, t}) => (
+const Component = ({i18n, model}) => (
   <SocialButtonsPane
+    instructions={i18n.html("socialLoginInstructions")}
     labelFn={i18n.str}
     lock={model}
     showLoading={true}
     signUp={false}
-    smallButtonsHeader={t("smallSocialButtonsHeader", {__textOnly: true})}
   />
 );
 
