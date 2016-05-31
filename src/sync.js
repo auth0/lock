40d511059e142fb18a4a6bb33138ce384a0465a5
Gabriel Andretta <gabriel.andretta@gmail.com>
2016-05-31 09:42:08 -0300

    Stop Lock after a sync error
    It will stop on every sync error unless it comes from a SSO data
    sync. Now it is hardcoded but it should be configurable.

--- src/sync.js
+++ src/sync.js
@@ -1,6 +1,7 @@
 import { Map } from 'immutable';
 import { dataFns } from './utils/data_utils';
 const { get, set } = dataFns(["sync"]);
+import * as l from './core/index';
 
 import { getEntity, observe, read, swap, updateEntity } from './store/index';
 
@@ -58,7 +59,7 @@ const process = (m, id) => {
           swap(updateEntity, "lock", id, m => {
             const recoverResult = getProp(m, k, "recoverResult");
             if (error && recoverResult === undefined) {
-              return setStatus(m, k, "error");
+              return handleError(m, k, error);
             } else {
               m = setStatus(m, k, "ok");
               return getProp(m, k, "successFn")(m, error ? recoverResult : result);
@@ -98,4 +99,15 @@ export function hasError(m, excludeKeys = []) {
 
 function isLoading(m, key) {
   return ["loading", "pending", "waiting"].indexOf(getStatus(m, key)) > -1;
+}
+
+function handleError(m, key, error) {
+  let result = setStatus(m, key, "error");
+
+  // TODO: this should be configurable for each sync
+  if (key !== "sso") {
+    result = l.stop(result);
+  }
+
+  return result;
 }
