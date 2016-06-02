5a52e84af32a2e3c832b8e6752aa87742f6bed39
Gabriel Andretta <gabriel.andretta@gmail.com>
2016-06-02 15:42:44 -0300

    Update passwordless email actions

--- src/connection/passwordless/actions.js
+++ src/connection/passwordless/actions.js
@@ -1,45 +1,36 @@
 import { Map } from 'immutable';
 import { read, getEntity, swap, updateEntity } from '../../store/index';
-import { closeLock } from '../../core/actions';
+import {
+  closeLock,
+  logIn as coreLogIn,
+  validateAndSubmit
+} from '../../core/actions';
 import webApi from '../../core/web_api';
 import * as c from '../../field/index';
 import * as l from '../../core/index';
 import * as m from './index';
 
 export function requestPasswordlessEmail(id) {
-  // TODO: abstract this submit thing.
-  swap(updateEntity, "lock", id, lock => {
-    if (c.isFieldValid(lock, "email")) {
-      return l.setSubmitting(lock, true);
-    } else {
-      return c.setFieldShowInvalid(lock, "email", true);
-    }
-  });
-
-  const lock = read(getEntity, "lock", id);
-
-  if (l.submitting(lock)) {
-    const isMagicLink = m.send(lock) === "link";
-    const options = {
-      email: c.email(lock),
-      send: m.send(lock),
+  validateAndSubmit(id, ["email"], m1 => {
+    const params = {
+      email: c.getFieldValue(m1, "email"),
+      send: m.send(m1)
     };
 
-    webApi.startPasswordless(id, options, error => {
+    webApi.startPasswordless(id, params, error => {
       if (error) {
         setTimeout(() => requestPasswordlessEmailError(id, error), 250);
       } else {
         requestPasswordlessEmailSuccess(id);
       }
     });
-  }
+  });
 }
 
 export function requestPasswordlessEmailSuccess(id) {
   swap(updateEntity, "lock", id, lock => {
     return m.setPasswordlessStarted(l.setSubmitting(lock, false), true);
   });
-  const lock = read(getEntity, "lock", id);
 }
 
 export function requestPasswordlessEmailError(id, error) {
@@ -119,72 +110,22 @@ export function resendEmail(id) {
 
 export function resendEmailSuccess(id) {
   swap(updateEntity, "lock", id, m.setResendSuccess);
-  const lock = read(getEntity, "lock", id);
 }
 
 export function resendEmailError(id, error) {
   swap(updateEntity, "lock", id, m.setResendFailed);
-  const lock = read(getEntity, "lock", id);
 }
 
 export function logIn(id) {
-  // TODO: abstract this submit thing
-  swap(updateEntity, "lock", id, lock => {
-    if (c.isFieldValid(lock, "vcode")) {
-      return l.setSubmitting(lock, true);
-    } else {
-      return c.setFieldShowInvalid(lock, "vcode", true);
-    }
-  });
-
-  const lock = read(getEntity, "lock", id);
-
-  if (l.submitting(lock)) {
-    const options = {
-      passcode: c.vcode(lock)
-    };
-
-    if (m.send(lock) === "sms") {
-      options.phoneNumber = c.fullPhoneNumber(lock);
-    } else {
-      options.email = c.email(lock);
-    }
-
-    webApi.logIn(
-      id,
-      options,
-      (error, ...args) => {
-        if (error) {
-          setTimeout(() => logInError(id, error), 250);
-        } else {
-          logInSuccess(id, ...args);
-        }
-      }
-    );
-  }
-}
-
-function logInSuccess(id, ...args) {
-  const lock = read(getEntity, "lock", id);
-  const autoclose = l.ui.autoclose(lock);
-
-  if (!autoclose) {
-    swap(updateEntity, "lock", id, lock => l.setLoggedIn(l.setSubmitting(lock, false), true));
-    l.invokeLogInCallback(lock, null, ...args);
+  const m1 = read(getEntity, "lock", id);
+  const params = {passcode: c.getFieldValue(m1, "vcode")};
+  if (m.send(m1) === "sms") {
+    params.phoneNumber = c.fullPhoneNumber(m1);
   } else {
-    closeLock(id, false, lock => l.invokeLogInCallback(lock, null, ...args));
+    params.email = c.getFieldValue(m1, "email");
   }
-}
 
-function logInError(id, error) {
-  const lock = read(getEntity, "lock", id);
-  const field = m.send(lock) === "sms" ? "phone number" : "email";
-  // TODO: update to new i18n API when bringing passwordless back
-  // const errorMessage = l.ui.t(lock, ["error", "signIn", error.error], {field: field, __textOnly: true}) || l.ui.t(lock, ["error", "signIn", "lock.request"], {field: field, __textOnly: true});
-  const errorMessage = "";
-  swap(updateEntity, "lock", id, l.setSubmitting, false, errorMessage);
-
-  l.invokeLogInCallback(lock, error);
+  coreLogIn(id, ["vcode"], params);
 }
 
 export function restart(id) {
