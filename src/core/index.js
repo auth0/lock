b485f80680da7645114d32dcb42a1f1ff41c5365
Gabriel Andretta <gabriel.andretta@gmail.com>
2016-06-06 18:00:45 -0300

    Don't invoke login callback when rendering stops

--- src/core/index.js
+++ src/core/index.js
@@ -339,7 +339,7 @@ export function loginErrorMessage(m, error, type) {
 // TODO: rename to something less generic that is easier to grep
 export function stop(m, error) {
   if (error) {
-    setTimeout(() => invokeLogInCallback(m, error), 0);
+    setTimeout(() => emitEvent(m, "error", error), 17);
   }
 
   return set(m, "stopped", true);
