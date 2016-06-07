8e77b761f242e969dba473229ff3e8874ce80299
Gabriel Andretta <gabriel.andretta@gmail.com>
2016-06-07 10:05:35 -0300

    Remove no longer used ask email screens

--- src/engine/passwordless.js
+++ src/engine/passwordless.js
@@ -15,7 +15,6 @@ import { isDone } from '../sync';
 import * as l from '../core/index';
 
 // import Base from './index';
-// import AskEmail from './connection/passwordless/ask_email';
 // import AskEmailVcode from './connection/passwordless/ask_email_vcode';
 // import AskSocialNetworkOrPhoneNumber from './field/or/ask_social_network_or_phone_number';
 // import AskPhoneNumber from './connection/passwordless/ask_phone_number';
