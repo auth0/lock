67d9203b88a9e0916398628804173ea6d4f19630
Gabriel Andretta <gabriel.andretta@gmail.com>
2016-06-07 10:07:39 -0300

    Remove ask email vcode screen

--- src/engine/passwordless.js
+++ src/engine/passwordless.js
@@ -15,7 +15,6 @@ import { isDone } from '../sync';
 import * as l from '../core/index';
 
 // import Base from './index';
-// import AskEmailVcode from './connection/passwordless/ask_email_vcode';
 // import AskSocialNetworkOrPhoneNumber from './field/or/ask_social_network_or_phone_number';
 // import AskPhoneNumber from './connection/passwordless/ask_phone_number';
 // import AskPhoneNumberVcode from './connection/passwordless/ask_phone_number_vcode';
