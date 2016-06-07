c3596a1f98ae44474ddd0bb9706a7e654fc6a7a8
Gabriel Andretta <gabriel.andretta@gmail.com>
2016-06-07 10:44:35 -0300

    Clean comments in passwordless engine

--- src/engine/passwordless.js
+++ src/engine/passwordless.js
@@ -14,12 +14,6 @@ import { initSocial } from '../connection/social/index';
 import { isDone } from '../sync';
 import * as l from '../core/index';
 
-// import Base from './index';
-// import MagiclinkScreen from './connection/passwordless/magiclink';
-// import { renderSSOScreens } from './core/sso/index';
-// import { isEmail } from './connection/passwordless/index';
-//
-//
 class Passwordless {
 
   didInitialize(m, opts) {
@@ -63,39 +57,6 @@ class Passwordless {
         : new SocialOrPhoneNumberLoginScreen();
     }
 
-    // const ssoScreen = renderSSOScreens(m);
-    // if (ssoScreen) return ssoScreen;
-
-    // const anySocialConnection = l.hasSomeConnections(m, "social");
-    // const anyPasswordlessConnection = l.hasSomeConnections(m, "passwordless");
-
-    // // social flow
-    // if (!anyPasswordlessConnection) {
-    //   return new AskSocialNetwork();
-    // }
-
-    // // social or magiclink flow, or magiclink flow
-    // // a link can be send only in an email
-    // if (isSendLink(m)) {
-    //   return anySocialConnection
-    //     ? new AskSocialNetworkOrEmail()
-    //     : new MagiclinkScreen();
-    // }
-
-    // // social or emailcode flow, or emailcode flow
-    // if (isEmail(m)) {
-    //   return passwordlessStarted(m)
-    //     ? new AskEmailVcode()
-    //     : (anySocialConnection ? new AskSocialNetworkOrEmail() : new AskEmail());
-    // }
-
-    // // social or sms flow, or sms flow
-    // return passwordlessStarted(m)
-    //   ? new AskPhoneNumberVcode()
-    //   : (anySocialConnection ? new AskSocialNetworkOrPhoneNumber() : new AskPhoneNumber());
-
-    // // TODO: show a crashed screen.
-    // throw new Error("unknown screen");
     setTimeout(() => {
       const stopError = new Error("Internal error");
       stopError.code = "internal_error";
@@ -108,87 +69,3 @@ class Passwordless {
 }
 
 export default new Passwordless();
-//
-// const dict = {
-//   email: {
-//     emailInputPlaceholder: "yours@example.com",
-//     headerText: "Enter your email to sign in<br>or create an account",
-//     terms: ""
-//   },
-//   emailCode: {
-//     codeInputPlaceholder: "Your code",
-//     headerText: "An email with the code has been sent to {email}.",
-//     resendLabel: "Did not get the code?"
-//   },
-//   emailSent: {
-//     failedLabel: "Failed!",
-//     retryLabel: "Retry",
-//     resendLabel: "Resend",
-//     resendingLabel: "Resending...",
-//     sentLabel: "Sent!",
-//     success: "We sent you a link to sign in<br />to {email}."
-//   },
-//   lastLogin: {
-//     headerText: "Last time you logged in with",
-//     skipLastLoginLabel: "Not your account?"
-//   },
-//   location: {
-//     locationFilterInputPlaceholder: "Select your country"
-//   },
-//   network: {
-//     headerText: "",
-//     loginWith: "Login with {idp}",
-//     smallSocialButtonsHeader: "Login with",
-//     terms: ""
-//   },
-//   networkOrEmail: {
-//     emailInputPlaceholder: "yours@example.com",
-//     headerText: "",
-//     loginWith: "Login with {idp}",
-//     separatorText: "Otherwise, enter your email to sign in<br>or create an account",
-//     smallSocialButtonsHeader: "Login with",
-//     terms: ""
-//   },
-//   networkOrPhone: {
-//     headerText: "",
-//     loginWith: "Login with {idp}",
-//     phoneNumberInputPlaceholder: "your phone number",
-//     separatorText: "Otherwise, enter your phone to sign in<br>or create an account",
-//     smallSocialButtonsHeader: "Login with",
-//     terms: ""
-//   },
-//   phone: {
-//     headerText: "Enter your phone to sign in<br>or create an account",
-//     phoneNumberInputPlaceholder: "your phone number",
-//     terms: ""
-//   },
-//   signedIn: {
-//     success: "Thanks for logging in."
-//   },
-//   smsCode: { // social or sms
-//     codeInputPlaceholder: "Your code",
-//     headerText: "An SMS with the code has been sent to {phoneNumber}.",
-//     resendLabel: "Did not get the code?"
-//   },
-//   error: {
-//     passwordless: {
-//       "bad.tenant": "We're sorry, we can't send you the {medium} because there's a configuration problem",
-//       "bad.client_id": "We're sorry, we can't send you the {medium} because there's a configuration problem",
-//       "bad.connection": "We're sorry, we can't send you the {medium} because there's a configuration problem",
-//       "bad.send": "We're sorry, we can't send you the {medium} because there's an internal error",
-//       "bad.authParams": "We're sorry, we can't send you the {medium} because there's an internal error",
-//       "bad.request": "We're sorry, we can't send you the {medium} because there's an internal error",
-//       "bad.email": "The email is invalid",
-//       "bad.phone_number": "The phone number is invalid",
-//       "lock.request": "We're sorry, something went wrong when sending the {medium}",
-//       "sms_provider_error": "We're sorry, something went wrong when sending the SMS",
-//       "sms_provider_error.bad_phone_number": "The number {phoneNumber} is not a valid phone number"
-//     },
-//     signIn: { // TODO: move this stuff to login, and namespace by connection
-//       "invalid_user_password": "Wrong {field} or verification code",
-//       "lock.popup_closed": "Popup window closed. Try again.",
-//       "lock.request": "We're sorry, something went wrong when attempting to sign in",
-//       "lock.unauthorized": "Permissions were not granted. Try again."
-//     },
-//   }
-// };
