7bf598f6d1ab072237788b90d2a5b537cfff267c
Benjamin Flores <beneliflo@gmail.com>
2016-08-03 17:10:38 -0300

    added no blur support in edge browser

--- src/ui/box/header.jsx
+++ src/ui/box/header.jsx
@@ -65,6 +65,9 @@ WelcomeMessage.propTypes = {
 
 const cssBlurSupport = (function() {
   // Check stolen from Modernizr, see https://github.com/Modernizr/Modernizr/blob/29eab707f7a2fb261c8a9c538370e97eb1f86e25/feature-detects/css/filters.js
+  const isEdge = global.navigator && !!global.navigator.userAgent.match(/Edge/i);
+  if (isEdge) return false;
+
   const el = global.document.createElement('div');
   el.style.cssText = "filter: blur(2px); -webkit-filter: blur(2px)";
   return !!el.style.length && (global.document.documentMode === undefined || global.document.documentMode > 9);
