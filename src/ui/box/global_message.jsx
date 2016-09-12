7d2812f4283dcaed112095e97200ac3eefab8dd0
Robbie WXYZ <robbiew.xyz@gmail.com>
2016-09-12 11:30:11 -0600

    use ReactCSSTransitionGroup for global messages (#595)
    * migrate global messages to ReactCSSTransitionGroup
    
    The existing code looked like quite a mess with its .style this and
    .style that.  This change lets the CSS do what it's good at and keeps
    the JS looking a bit cleaner in that file.
    
    It also cuts down on a dependency.

--- src/ui/box/global_message.jsx
+++ src/ui/box/global_message.jsx
@@ -14,57 +14,6 @@ export default class GlobalMessage extends React.Component {
     );
   }
 
-  // componentWillAppear(callback) {
-  //   console.log("componentWillAppear")
-  //   callback();
-  // }
-  //
-  // componentDidAppear() {
-  //   console.log("componentDidAppear");
-  // }
-
-  componentWillEnter(callback) {
-    // console.log("componentWillEnter");
-    const node = ReactDOM.findDOMNode(this);
-    var computedStyle = window.getComputedStyle(node, null);
-    var height = computedStyle.height;
-    var paddingTop = computedStyle.paddingTop;
-    var paddingBottom = computedStyle.paddingBottom;
-    node.style.height = "0px";
-    node.style.paddingTop = "0px";
-    node.style.paddingBottom = "0px";
-    setTimeout(function() {
-      node.style.transition = "all 0.2s";
-      node.style.height = "";
-      node.style.paddingTop = "";
-      node.style.paddingBottom = "";
-      callback();
-    }, 17);
-  }
-
-  // componentDidEnter() {
-  //   console.log("componentDidEnter");
-  // }
-
-  componentWillLeave(callback) {
-    // console.log("componentWillLeave");
-    const node = ReactDOM.findDOMNode(this);
-    node.style.transition = "all 0.2s";
-    node.style.height = "0px";
-    node.style.paddingTop = "0px";
-    node.style.paddingBottom = "0px";
-    setTimeout(function() {
-      node.style.removeProperty("transition");
-      node.style.removeProperty("height");
-      node.style.removeProperty("padding-top");
-      node.style.removeProperty("padding-bottom");
-      callback();
-    }, 250);
-  }
-
-  // componentDidLeave() {
-  //   console.log("componentDidLeave");
-  // }
 }
 
 GlobalMessage.propTypes = {
