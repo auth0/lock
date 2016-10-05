5d203b0ed6407cc0e744d6cd44b4e983bf2f70ee
Stefan Lau <stefan.lau@holidaycheck.com>
2016-10-05 10:50:30 +0200

    Inject lock styles when lock is instantiated

--- src/index.js
+++ src/index.js
@@ -3,27 +3,31 @@ import Core from './core';
 import classic from './engine/classic';
 import css from '../css/index.css';
 
-const styleId = "auth0-lock-style";
-let style = document.getElementById(styleId);
+function injectStyles() {
+  const styleId = "auth0-lock-style";
+  let style = document.getElementById(styleId);
 
-if (!style) {
-  const head = document.getElementsByTagName("head")[0];
-  style = document.createElement("style");
-  style.type = "text/css";
-  style.setAttribute("id", styleId);
-  head.appendChild(style);
-}
+  if (!style) {
+    const head = document.getElementsByTagName("head")[0];
+    style = document.createElement("style");
+    style.type = "text/css";
+    style.setAttribute("id", styleId);
+    head.appendChild(style);
+  }
 
-if (style.styleSheet) {
-  style.styleSheet.cssText = css;
-} else {
-  style.innerHTML = css;
+  if (style.styleSheet) {
+    style.styleSheet.cssText = css;
+  } else {
+    style.innerHTML = css;
+  }
 }
 
 export default class Auth0Lock extends Core {
 
   constructor(clientID, domain, options) {
     super(clientID, domain, options, classic);
+
+    injectStyles();
   }
 
 }
