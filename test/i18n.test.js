f10be5a7a708c3ac5f8ea080c8aaec723078cb4d
German Lena <german.lena@gmail.com>
2016-10-26 10:20:28 -0300

    working with stub/sinon

--- test/i18n.test.js
+++ test/i18n.test.js
@@ -1,21 +1,32 @@
+"use strict";
+import { stub } from 'sinon';
+
 import expect from 'expect.js';
 import Immutable from 'immutable';
-import {initI18n} from '../src/i18n';
-import { go } from '../src/sync';
-import { swap, setEntity, updateEntity, observe } from '../src/store/index';
-import { dataFns } from '../src/utils/data_utils';
+import flatten from 'flat';
 
 import enDictionary from '../src/i18n/en';
 import esDictionary from '../src/i18n/es';
 
-const core = dataFns(["core"]);
+import * as sync from '../src/sync';
 
-describe.skip("load i18n configuration", function() {
+stub(sync, "default", (m, key, opts) => {
+  m = opts.successFn(m, esDictionary);
+  return m;
+});
 
-  it("should merge and warn missing keys", function(done) {
+import { initI18n } from '../src/i18n';
 
-    const id = 1;
 
+describe.only("load i18n configuration", () => {
+
+  after(function() {
+    sync.default.restore();
+  });
+
+  it('should merge and warn missing keys', () => {
+
+    // We need to initialize the state
     var m = Immutable.fromJS({
       languageBaseUrl: "https://cdn.auth0.com",
       ui: {
@@ -23,32 +34,19 @@ describe.skip("load i18n configuration", function() {
         language: "es"
       }
     });
-    
-    go(id);
 
-    observe("test", id, (m) => { 
-      if (m.getIn(['sync', 'i18n', 'syncStatus']) === 'ok') {
-        assertLanguage(m.getIn(['i18n', 'strings']).toJS(), enDictionary, esDictionary)
-        done();
-      }
-    });
-    
-    m = core.init(id, m);
-
+    // Initialize i18n.
     m = initI18n(m);
 
-    m = swap(setEntity, "lock", id, m);
+    let language = flatten(m.getIn(['i18n', 'strings']).toJS());
+    let en = flatten(enDictionary);
+    let es = flatten(esDictionary);
+
+    // We should check that the language has all the keys in the 
+    // en language and its values should be either es or en.
+    Object.keys(en).forEach( (key) => {
+      expect(language).to.have.property(key);
+      expect([en[key], es[key]]).to.contain(language[key]);
+    })
   });
 });
-
-function assertLanguage(language, en, es) { 
-  Object.keys(en).forEach( (key) => {
-    expect(language).to.have.property(key);
-    if (typeof en[key] === 'object') {
-      assertLanguage(language[key], en[key], es[key]);
-    }
-    else {
-      expect([en[key], es[key]]).to.contain(language[key]);
-    }
-  });
-}
