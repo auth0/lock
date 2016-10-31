884ef18baf1bd1dda66110bf69533b74117a54db
Stefan Lau <selaux@users.noreply.github.com>
2016-10-31 16:12:54 +0100

    Isomorphic fixes (#631)
    * Inject lock styles when lock is instantiated
    
    * Dont throw when document is undefined
    
    * Attach css to Auth0Lock constructor

--- src/index.js
+++ src/index.js
@@ -3,33 +3,38 @@ import Core from './core';
 import classic from './engine/classic';
 import css from '../css/index.css';
 
-const styleId = "auth0-lock-style";
-let style = document.getElementById(styleId);
+function injectStyles() {
+  const styleId = "auth0-lock-style";
+  let style = document.getElementById(styleId);
 
-if (!style) {
-  const head = document.getElementsByTagName("head")[0];
-  style = document.createElement("style");
-  style.type = "text/css";
-  style.setAttribute("id", styleId);
-  head.appendChild(style);
-}
+  if (!style) {
+    const head = document.getElementsByTagName("head")[0];
+    style = document.createElement("style");
+    style.type = "text/css";
+    style.setAttribute("id", styleId);
+    head.appendChild(style);
+  }
 
-if (style.styleSheet) {
-  style.styleSheet.cssText = css;
-} else {
-  style.innerHTML = css;
+  if (style.styleSheet) {
+    style.styleSheet.cssText = css;
+  } else {
+    style.innerHTML = css;
+  }
 }
 
 export default class Auth0Lock extends Core {
 
   constructor(clientID, domain, options) {
     super(clientID, domain, options, classic);
+
+    injectStyles();
   }
 
 }
 
 // telemetry
 Auth0Lock.version = __VERSION__;
+Auth0Lock.css = css;
 Auth0.clientInfo.lib_version = Auth0.clientInfo.version;
 Auth0.clientInfo.name =  "lock.js";
 Auth0.clientInfo.version = Auth0Lock.version;
