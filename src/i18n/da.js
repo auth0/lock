e13720f5ea53fd1951225e03cfad580bf921a428
German Lena <german.lena@gmail.com>
2017-01-16 10:50:51 -0300

    Preprocess translations

--- src/i18n/da.js
+++ src/i18n/da.js
@@ -1,10 +1,13 @@
+// This file was automatically translated.
+// Feel free to submit a PR if you find a more accurate translation.
+
 export default {
-  error: {
-    forgotPassword: {
+  "error": {
+    "forgotPassword": {
       "too_many_requests": "Du har nået grænsen for forsøg på at skifte kodeord. Vent venligst før du prøver igen.",
       "lock.fallback": "Vi beklager, men der skete en i forespørgslen efter nyt kodeord."
     },
-    login: {
+    "login": {
       "blocked_user": "Denne bruger er blokeret.",
       "invalid_user_password": "Forkerte loginoplysninger.",
       "lock.fallback": "Vi beklager, men der skete en fejl i forbindelse med login.",
@@ -14,16 +17,16 @@ export default {
       "lock.network": "Vi kunne for få forbindelse til serveren. Kontroller venligst din forbindelse og prøv igen.",
       "lock.popup_closed": "Popup-vinduet er lukket. Prøv venligst igen.",
       "lock.unauthorized": "Tilladelse blev ikke tildelt. Prøv igen.",
-      "password_change_required": "Du skal opdatere din adgangskode, fordi det er første gang du logger på, eller fordi din adgangskode er udløbet.", // TODO: verify error code
+      "password_change_required": "Du skal opdatere din adgangskode, fordi det er første gang du logger på, eller fordi din adgangskode er udløbet.",
       "password_leaked": "Dette login er blevet blokeret, fordi din adgangskode er blevet lækket på en anden hjemmeside. Vi har sendt dig en e-mail med instruktioner om, hvordan du fjerner blokeringen.",
       "too_many_attempts": "Din konto er blevet blokeret efter gentagne mislykkede login-forsøg."
     },
-    passwordless: {
+    "passwordless": {
       "bad.email": "Denne email er ugyldig",
       "bad.phone_number": "Dette telefonnummer er ugyldigt",
       "lock.fallback": "Vi beklager, men der skete en fejl"
     },
-    signUp: {
+    "signUp": {
       "invalid_password": "Kodeordet er ugyldigt",
       "lock.fallback": "Vi beklager, men der skete en fejl, da du forsøgte at oprette dig.",
       "password_dictionary_error": "Kodeordet er for almindeligt.",
@@ -33,70 +36,70 @@ export default {
       "username_exists": "Dette brugernavn eksisterer allerede."
     }
   },
-  success: { // success messages show above the form or in a confirmation pane
-    logIn: "Tak fordi du loggede ind.",
-    forgotPassword: "Vi har sendt den en mail med instruktioner til at nulstille dit kodeord.",
-    magicLink: "Vi har sendt dig et link, som du kan bruge til at logge ind<br />i %s.",
-    signUp: "Tak fordi du oprettede en bruger."
+  "success": {
+    "logIn": "Tak fordi du loggede ind.",
+    "forgotPassword": "Vi har sendt den en mail med instruktioner til at nulstille dit kodeord.",
+    "magicLink": "Vi har sendt dig et link, som du kan bruge til at logge ind<br />i %s.",
+    "signUp": "Tak fordi du oprettede en bruger."
   },
-  blankErrorHint: "Kan ikke være tom",
-  codeInputPlaceholder: "din kode",
-  databaseEnterpriseLoginInstructions: "",
-  databaseEnterpriseAlternativeLoginInstructions: "eller",
-  databaseSignUpInstructions: "",
-  databaseAlternativeSignUpInstructions: "eller",
-  emailInputPlaceholder: "dit@eksempel.dk",
-  enterpriseLoginIntructions: "Log ind med dit login til din virksomhed.",
-  enterpriseActiveLoginInstructions: "Indtast venligst dit login hos %s.",
-  failedLabel: "Mislykkede!",
-  forgotPasswordAction: "Har du glemt dit kodeord?",
-  forgotPasswordInstructions: "Indtast venligst din email, så sender vi instruktioner til at nulstille dit kodeord.",
-  forgotPasswordSubmitLabel: "Send email",
-  invalidErrorHint: "Ugyldig",
-  lastLoginInstructions: "Sidste gang loggede du ind med",
-  loginAtLabel: "Log ind hos %s",
-  loginLabel: "Log Ind",
-  loginSubmitLabel: "Log Ind",
-  loginWithLabel: "Log ind med %s",
-  notYourAccountAction: "Er det ikke din konto?",
-  passwordInputPlaceholder: "dit kodeord",
-  passwordStrength: {
-    containsAtLeast: "Skal indeholde mindt %d af de følgende %d typer af karakterer:",
-    identicalChars: "Ikke mere end %d identiske karakterer efter hinanden (f.eks., \"%s\" er ikke tilladt)",
-    nonEmpty: "Ikke-tomt kodeord er påkrævet",
-    numbers: "Numre (f.eks. 0-9)",
-    lengthAtLeast: "Mindst %d karakterer langt",
-    lowerCase: "Små bogstaver (a-z)",
-    shouldContain: "Skal indeholde:",
-    specialCharacters: "Specielle karakterer (f.eks. !@#$%^&*)",
-    upperCase: "Store bogstaver (A-Z)"
+  "blankErrorHint": "Kan ikke være tom",
+  "codeInputPlaceholder": "din kode",
+  "databaseEnterpriseLoginInstructions": "",
+  "databaseEnterpriseAlternativeLoginInstructions": "eller",
+  "databaseSignUpInstructions": "",
+  "databaseAlternativeSignUpInstructions": "eller",
+  "emailInputPlaceholder": "dit@eksempel.dk",
+  "enterpriseLoginIntructions": "Log ind med dit login til din virksomhed.",
+  "enterpriseActiveLoginInstructions": "Indtast venligst dit login hos %s.",
+  "failedLabel": "Mislykkede!",
+  "forgotPasswordAction": "Har du glemt dit kodeord?",
+  "forgotPasswordInstructions": "Indtast venligst din email, så sender vi instruktioner til at nulstille dit kodeord.",
+  "forgotPasswordSubmitLabel": "Send email",
+  "invalidErrorHint": "Ugyldig",
+  "lastLoginInstructions": "Sidste gang loggede du ind med",
+  "loginAtLabel": "Log ind hos %s",
+  "loginLabel": "Log Ind",
+  "loginSubmitLabel": "Log Ind",
+  "loginWithLabel": "Log ind med %s",
+  "notYourAccountAction": "Er det ikke din konto?",
+  "passwordInputPlaceholder": "dit kodeord",
+  "passwordStrength": {
+    "containsAtLeast": "Skal indeholde mindt %d af de følgende %d typer af karakterer:",
+    "identicalChars": "Ikke mere end %d identiske karakterer efter hinanden (f.eks., \"%s\" er ikke tilladt)",
+    "nonEmpty": "Ikke-tomt kodeord er påkrævet",
+    "numbers": "Numre (f.eks. 0-9)",
+    "lengthAtLeast": "Mindst %d karakterer langt",
+    "lowerCase": "Små bogstaver (a-z)",
+    "shouldContain": "Skal indeholde:",
+    "specialCharacters": "Specielle karakterer (f.eks. !@#$%^&*)",
+    "upperCase": "Store bogstaver (A-Z)"
   },
-  passwordlessEmailAlternativeInstructions: "Ellers, indtast din email for at logge ind<br/>eller oprette en konto",
-  passwordlessEmailCodeInstructions: "En email med koden er sendt til %s.",
-  passwordlessEmailInstructions: "Indtast din email for at logge ind<br/>eller oprette en konto",
-  passwordlessSMSAlternativeInstructions: "Ellers, indtast dit telefonnummer for at logge ind<br/>eller oprette en konto",
-  passwordlessSMSCodeInstructions: "En SMS med kode er sendt<br/>til %s.",
-  passwordlessSMSInstructions: "Indtast dit telefonnummer for at logge ind<br/>eller oprette en konto",
-  phoneNumberInputPlaceholder: "dit telefonnummer",
-  resendCodeAction: "Har du ikke modtaget koden?",
-  resendLabel: "Send igen",
-  resendingLabel: "Sender igen...",
-  retryLabel: "Prøv igen",
-  sentLabel: "Sendt!",
-  signUpLabel: "Opret dig",
-  signUpSubmitLabel: "Opret dig",
-  signUpTerms: "",
-  signUpWithLabel: "Opret dig med %s",
-  socialLoginInstructions: "",
-  socialSignUpInstructions: "",
-  ssoEnabled: "Single Sign-On aktiveret",
-  submitLabel: "Send",
-  unrecoverableError: "Der skete en fejl.<br />Kontakt venligst den tekniske support.",
-  usernameFormatErrorHint: "Brug %d-%d bogstaver, numre og \"_\"",
-  usernameInputPlaceholder: "dit brugernavn",
-  usernameOrEmailInputPlaceholder: "brugernavn/email",
-  title: "Auth0",
-  welcome: "Velkommen %s!",
-  windowsAuthInstructions: "Du er forbundet fra din virksomheds netværk&hellip;",
-  windowsAuthLabel: "Windows Authentication"
-};
+  "passwordlessEmailAlternativeInstructions": "Ellers, indtast din email for at logge ind<br/>eller oprette en konto",
+  "passwordlessEmailCodeInstructions": "En email med koden er sendt til %s.",
+  "passwordlessEmailInstructions": "Indtast din email for at logge ind<br/>eller oprette en konto",
+  "passwordlessSMSAlternativeInstructions": "Ellers, indtast dit telefonnummer for at logge ind<br/>eller oprette en konto",
+  "passwordlessSMSCodeInstructions": "En SMS med kode er sendt<br/>til %s.",
+  "passwordlessSMSInstructions": "Indtast dit telefonnummer for at logge ind<br/>eller oprette en konto",
+  "phoneNumberInputPlaceholder": "dit telefonnummer",
+  "resendCodeAction": "Har du ikke modtaget koden?",
+  "resendLabel": "Send igen",
+  "resendingLabel": "Sender igen...",
+  "retryLabel": "Prøv igen",
+  "sentLabel": "Sendt!",
+  "signUpLabel": "Opret dig",
+  "signUpSubmitLabel": "Opret dig",
+  "signUpTerms": "",
+  "signUpWithLabel": "Opret dig med %s",
+  "socialLoginInstructions": "",
+  "socialSignUpInstructions": "",
+  "ssoEnabled": "Single Sign-On aktiveret",
+  "submitLabel": "Send",
+  "unrecoverableError": "Der skete en fejl.<br />Kontakt venligst den tekniske support.",
+  "usernameFormatErrorHint": "Brug %d-%d bogstaver, numre og \"_\"",
+  "usernameInputPlaceholder": "dit brugernavn",
+  "usernameOrEmailInputPlaceholder": "brugernavn/email",
+  "title": "Auth0",
+  "welcome": "Velkommen %s!",
+  "windowsAuthInstructions": "Du er forbundet fra din virksomheds netværk&hellip;",
+  "windowsAuthLabel": "Windows Authentication"
+}
