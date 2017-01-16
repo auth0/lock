f85467089b4a3dbef0ca13c8ac8adb89cbff298b
leplay <leqcany@gmail.com>
2017-01-16 21:16:28 +0800

    Update zh.js (#774)
    * Update zh.js

--- src/i18n/zh.js
+++ src/i18n/zh.js
@@ -7,7 +7,7 @@ export default {
     login: {
       "blocked_user": "该账号已被锁定。",
       "invalid_user_password": "密码错误",
-      "lock.fallback": "对不起，请求登陆时出现错误。",
+      "lock.fallback": "对不起，请求登录时出现错误。",
       "lock.invalid_code": "代码错误。",
       "lock.invalid_email_password": "邮箱或密码错误。",
       "lock.invalid_username_password": "账号或密码错误。",
@@ -36,7 +36,7 @@ export default {
   success: {
     logIn: "登录成功",
     forgotPassword: "重置密码的邮件已发送",
-    magicLink: "已向您发送链接<br />到 %s 登录", // This one needs review
+    magicLink: "已向您发送链接<br/>到 %s 登录", // This one needs review
     signUp: "感谢您的注册。"
   },
   blankErrorHint: "不能为空",
@@ -53,30 +53,30 @@ export default {
   forgotPasswordInstructions: "请输入您的邮箱，我们将为你发送重置密码的邮件。",
   forgotPasswordSubmitLabel: "发电子邮件", // needs review
   invalidErrorHint: "错误",
-  lastLoginInstructions: "上次登陆的信息为",
+  lastLoginInstructions: "上次登录的信息为",
   loginAtLabel: "登录到 %s",
   loginLabel: "登录",
   loginSubmitLabel: "登录", // needs review
-  loginWithLabel: "用 %s 登录",
+  loginWithLabel: "使用 %s 登录",
   notYourAccountAction: "不是您的账号?",
   passwordInputPlaceholder: "您的密码",
   passwordStrength: {
-    containsAtLeast: "至少包含%d个以下%d种字符:",
-    identicalChars: "不能多于%d个相同的字符在同一行(例如,不允许出现 \"%s\" )",
+    containsAtLeast: "至少包含 %d 个以上 %d 种字符:",
+    identicalChars: "不能多于 %d 个相同的字符在同一行(例如,不允许出现 \"%s\" )",
     nonEmpty: "密码不能为空",
     numbers: "数字 (如 0-9)",
-    lengthAtLeast: "最少长度为%d个字符",
-    lowerCase: "小写字母(a-z)",
-    shouldContain: "应包含:",
+    lengthAtLeast: "最少长度为 %d 个字符",
+    lowerCase: "小写字母 (a-z)",
+    shouldContain: "应包含：",
     specialCharacters: "特殊字符 (如 !@#$%^&*)",
-    upperCase: "大写字母(A-Z)"
+    upperCase: "大写字母 (A-Z)"
   },
-  passwordlessEmailAlternativeInstructions: "您还可以通过邮箱登录<br>或者创建账号", // This one needs review
+  passwordlessEmailAlternativeInstructions: "您还可以通过邮箱登录<br/>或者创建账号", // This one needs review
   passwordlessEmailCodeInstructions: "代码已通过邮件发送到 %s。",
-  passwordlessEmailInstructions: "输入邮箱登录<br>或者创建账号。",
-  passwordlessSMSAlternativeInstructions: "您还可以通过手机号码登录<br>或者创建账号。",
-  passwordlessSMSCodeInstructions: "代码已通过短信发送到<br> %s。",
-  passwordlessSMSInstructions: "输入手机号码登录<br>或者创建账号",
+  passwordlessEmailInstructions: "输入邮箱登录<br/>或者创建账号。",
+  passwordlessSMSAlternativeInstructions: "您还可以通过手机号码登录<br/>或者创建账号。",
+  passwordlessSMSCodeInstructions: "代码已通过短信发送到<br/> %s。",
+  passwordlessSMSInstructions: "输入手机号码登录<br/>或者创建账号",
   phoneNumberInputPlaceholder: "您的手机号码",
   resendCodeAction: "没有收到号码?",
   resendLabel: "重新发送",
@@ -91,12 +91,17 @@ export default {
   socialSignUpInstructions: "",
   ssoEnabled: "单点登录已激活",
   submitLabel: "提交", // needs review
-  unrecoverableError: "出现错误。<br />请联系技术人员。",
-  usernameFormatErrorHint: "请使用%d-%d个字母, 数字或 \"_\"的组合",
+  unrecoverableError: "出现错误。<br/>请联系技术人员。",
+  usernameFormatErrorHint: "请使用 %d-%d 位字母、数字或 \"_\"的组合",
   usernameInputPlaceholder: "您的用户名",
   usernameOrEmailInputPlaceholder: "用户名/邮箱",
   title: "Auth0",
   welcome: "欢迎 %s!",
   windowsAuthInstructions: "您已连接到组织网络&hellip;",
-  windowsAuthLabel: "Windows认证"
+  windowsAuthLabel: "Windows 认证",
+  mfaInputPlaceholder: "验证码",
+  mfaLoginTitle: "两步验证",
+  mfaLoginInstructions: "请输入您手机应用上显示的验证码。",
+  mfaSubmitLabel: "登录",
+  mfaCodeErrorHint: "使用 %d 位数字"
 };
