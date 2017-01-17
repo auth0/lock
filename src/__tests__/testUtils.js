c61b25c2ae9233daf2d92b70454974a5898e93ee
Luis Rudge <luis@luisrudge.net>
2017-01-17 18:01:45 -0300

    changing the way mockedComponent renders its props

--- src/__tests__/testUtils.js
+++ src/__tests__/testUtils.js
@@ -8,4 +8,21 @@ export const expectComponent = (children) => {
 
 export const noop = () => {};
 
-export const mockComponent = type => (...props) => <div data-type={type}>{JSON.stringify(props)}</div>;
+const addDataToProps = (props) => {
+  const returnedProps = {};
+  Object.keys(props).forEach(k => returnedProps[`data-${k}`] = props[k]);
+  return returnedProps;
+};
+
+const removeDataFromProps = (props) => {
+  const returnedProps = {};
+  Object.keys(props).forEach(k => returnedProps[k.replace('data-', '')] = props[k]);
+  return returnedProps;
+};
+
+export const mockComponent = (type, domElement = 'div') => (props) => React.createElement(domElement, {
+  'data-__type': type,
+  ...addDataToProps(props)
+});
+
+export const extractPropsFromWrapper = wrapper => removeDataFromProps(wrapper.find('div').props());
