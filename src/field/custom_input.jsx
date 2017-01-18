71ff5f47e4d21077fe1a9284e93a9b4411718031
Luis Deschamps Rudge <luis@admins-MacBook-Pro.local>
2017-01-18 20:47:34 -0300

    custom input WIP

--- src/field/custom_input.jsx
+++ src/field/custom_input.jsx
@@ -10,7 +10,7 @@ import TextInput from '../ui/input/text_input';
 import SelectInput from '../ui/input/select_input';
 import * as l from '../core/index';
 
-const CustomInput = ({iconUrl, model, name, options, placeholder, type, validator}) => {
+const CustomInput = ({iconUrl, model, name, placeholder, type, validator}) => {
   const props = {
     iconUrl,
     isValid: !isFieldVisiblyInvalid(model, name),
