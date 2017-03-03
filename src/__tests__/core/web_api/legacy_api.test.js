612ad3cf936ec51c1e6f70eb24474baa7d08eeec
Hernan Zalazar <hernan.zalazar@gmail.com>
2017-03-03 16:14:28 -0300

    Add owp option in non oidc webapi

--- src/__tests__/core/web_api/legacy_api.test.js
+++ src/__tests__/core/web_api/legacy_api.test.js
@@ -24,6 +24,7 @@ describe('Auth0LegacyAPIClient', () => {
   beforeEach(() => {
     jest.resetModules();
   });
+
   describe('logIn', () => {
     const assertCallWithCallback = (mock, callbackFunction) => {
       expect(mock.calls.length).toBe(1);
@@ -31,63 +32,115 @@ describe('Auth0LegacyAPIClient', () => {
       mock.calls[0][1]();
       expect(callbackFunction.mock.calls.length).toBe(1);
     };
+
+    const assertMethodCall = (method) => {
+      expect(method.calls.length).toBe(1);
+      expect(method.calls[0][0]).toMatchSnapshot();
+    };
+
+    const assertCallbackIsCalledOnce = (callback, method) => {
+      method.calls[0][1]();
+      expect(callback.mock.calls.length).toBe(1);
+    };
+
+    var callback;
+
+    beforeEach(() => {
+      callback = jest.fn();
+    });
+
     describe('with social/enterprise (without username and email)', () => {
+      const redirectAuthorize = () => {
+        const mock = getAuth0ClientMock();
+        return mock.WebAuth.mock.instances[0].authorize.mock;
+      };
+
+      const popupAuthorize = () => {
+        const mock = getAuth0ClientMock();
+        return mock.WebAuth.mock.instances[0].popup.authorize.mock;
+      };
+
       it('should call authorize when redirect===true', () => {
         const client = getClient({
           redirect: true
         });
-        const callback = jest.fn();
         client.logIn({}, {}, callback);
-        const mock = getAuth0ClientMock();
-        const authorizeMock = mock.WebAuth.mock.instances[0].authorize.mock;
-        assertCallWithCallback(authorizeMock, callback);
+        assertMethodCall(redirectAuthorize());
+        assertCallbackIsCalledOnce(callback, redirectAuthorize());
       });
+
       it('should call popup.authorize when redirect===false', () => {
         const client = getClient({
           redirect: false
         });
-        const callback = jest.fn();
         client.logIn({}, {}, callback);
-        const mock = getAuth0ClientMock();
-        const authorizeMock = mock.WebAuth.mock.instances[0].popup.authorize.mock;
-        assertCallWithCallback(authorizeMock, callback);
+        assertMethodCall(popupAuthorize());
+        assertCallbackIsCalledOnce(callback, popupAuthorize());
+      });
+
+      it('should call popup.authorize with additional authParams', () => {
+        const client = getClient({
+          redirect: false
+        });
+        client.logIn({}, { scope: 'openid offline_access read:users', audience: 'https://mysite.com/api'}, callback);
+        assertMethodCall(popupAuthorize());
+        assertCallbackIsCalledOnce(callback, popupAuthorize());
       });
     });
+
     describe('with sso and redirect === false', () => {
       it('should call loginWithResourceOwner', () => {
         const client = getClient({
           sso: false,
           redirect: false
         });
-        const callback = jest.fn();
         client.logIn({ username: 'foo' }, {}, callback);
         const mock = getAuth0ClientMock();
         const loginWithResourceOwnerMock = mock.WebAuth.mock.instances[0].client.loginWithResourceOwner.mock;
-        assertCallWithCallback(loginWithResourceOwnerMock, callback);
+        assertMethodCall(loginWithResourceOwnerMock);
+        assertCallbackIsCalledOnce(callback, loginWithResourceOwnerMock);
       });
     });
+
     describe('with credentials', () => {
+      const redirectLoginWithCredentials = () => {
+        const mock = getAuth0ClientMock();
+        return mock.WebAuth.mock.instances[0].redirect.loginWithCredentials.mock;
+      };
+
+      const popupLoginWithCredentials = () => {
+        const mock = getAuth0ClientMock();
+        return mock.WebAuth.mock.instances[0].popup.loginWithCredentials.mock;
+      };
+
       it('should call loginWithCredentials', () => {
         const client = getClient({
           redirect: true,
           sso: true
         });
-        const callback = jest.fn();
         client.logIn({ username: 'foo' }, {}, callback);
-        const mock = getAuth0ClientMock();
-        const authorizeMock = mock.WebAuth.mock.instances[0].redirect.loginWithCredentials.mock;
-        assertCallWithCallback(authorizeMock, callback);
+        assertMethodCall(redirectLoginWithCredentials());
+        assertCallbackIsCalledOnce(callback, redirectLoginWithCredentials());
       });
+
+      it('should call loginWithCredentials with auth params', () => {
+        const client = getClient({
+          redirect: true,
+          sso: true
+        });
+        client.logIn({ username: 'foo' }, { scope: 'openid offline_access write:users', audience: 'https://mysites.com/api' }, callback);
+        assertMethodCall(redirectLoginWithCredentials());
+        assertCallbackIsCalledOnce(callback, redirectLoginWithCredentials());
+      });
+
       it('should call popup.authorize when redirect===false', () => {
         const client = getClient({
           redirect: false,
           sso: true
         });
-        const callback = jest.fn();
         client.logIn({ username: 'foo' }, {}, callback);
-        const mock = getAuth0ClientMock();
-        const authorizeMock = mock.WebAuth.mock.instances[0].popup.loginWithCredentials.mock;
-        assertCallWithCallback(authorizeMock, callback);
+        assertMethodCall(popupLoginWithCredentials());
+        assertCallbackIsCalledOnce(callback, popupLoginWithCredentials());
       });
     });
   });
