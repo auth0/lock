e0fb226542426679e5cac00e1859d2341f0e412c
Luis Rudge <luis@luisrudge.net>
2017-03-28 16:49:59 -0300

    adding `key` to the error "An error occurred when fetching data"

--- src/sync.js
+++ src/sync.js
@@ -116,7 +116,7 @@ function handleError(m, key, error) {
 
   // TODO: this should be configurable for each sync
   if (key !== "sso") {
-    const stopError = new Error("An error occurred when fetching data.");
+    const stopError = new Error(`An error occurred when fetching data. (key: ${key})`);
     stopError.code = "sync";
     stopError.origin = error;
     result = l.stop(result, stopError);
