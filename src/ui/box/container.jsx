d387f58113c20c8adf8d36af6ef2fe5110c59490
Ramiro Silveyra d'Avila <ramasilveyra1@gmail.com>
2017-05-04 20:24:54 -0300

    Change styleguide image urls to the latest version that also has cache

--- src/ui/box/container.jsx
+++ src/ui/box/container.jsx
@@ -258,7 +258,7 @@ export const defaultProps = (Container.defaultProps = {
   disableSubmitButton: false,
   isMobile: false,
   isSubmitting: false,
-  logo: `${isFileProtocol ? 'https:' : ''}//cdn.auth0.com/styleguide/1.0.0/img/badge.png`,
+  logo: `${isFileProtocol ? 'https:' : ''}//cdn.auth0.com/styleguide/components/1.0.8/media/logos/img/badge.png`,
   primaryColor: '#ea5323',
   showBadge: true
 });
