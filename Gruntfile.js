f6622f313a3e07f5f249deda2553c0f9827792f0
Luis Rudge <luis@luisrudge.net>
2017-05-25 13:43:30 -0300

    Added Cross Origin Auth support to the login method

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,31 +1,30 @@
-"use strict";
+'use strict';
 
-const path = require("path");
-const fs = require("fs");
-const pkg = require("./package");
-const webpack = require("webpack");
-const webpackConfig = require("./webpack.config.js");
-const UnminifiedWebpackPlugin = require("unminified-webpack-plugin");
+const path = require('path');
+const fs = require('fs');
+const pkg = require('./package');
+const webpack = require('webpack');
+const webpackConfig = require('./webpack.config.js');
+const UnminifiedWebpackPlugin = require('unminified-webpack-plugin');
 
-module.exports = function (grunt) {
-
-  const pkg_info = grunt.file.readJSON("package.json");
+module.exports = function(grunt) {
+  const pkg_info = grunt.file.readJSON('package.json');
 
   grunt.initConfig({
     pkg: pkg_info,
     clean: {
-      build: ["build/"],
-      dev: ["build/"],
-      dist: ["lib/"]
+      build: ['build/'],
+      dev: ['build/'],
+      dist: ['lib/']
     },
     babel: {
       dist: {
         files: [
           {
             expand: true,
-            cwd: "src",
-            src: ["**/*.js", "**/*.jsx"],
-            dest: "lib",
+            cwd: 'src',
+            src: ['**/*.js', '**/*.jsx'],
+            dest: 'lib',
             ext: '.js'
           }
         ]
@@ -33,22 +32,21 @@ module.exports = function (grunt) {
     },
     env: {
       build: {
-        NODE_ENV: "production"
+        NODE_ENV: 'production'
       }
     },
     exec: {
-      touch_index: "touch src/index.js"
+      touch_index: 'touch src/index.js'
     },
     webpack: {
       options: webpackConfig,
       build: {
-        devtool: "source-map",
+        devtool: 'source-map',
         output: {
-          path: path.join(__dirname, "build"),
+          path: path.join(__dirname, 'build'),
           filename: 'lock.min.js'
         },
         watch: false,
-        devtool: 'source-map',
         plugins: [
           new webpack.LoaderOptionsPlugin({
             minimize: true,
@@ -56,7 +54,7 @@ module.exports = function (grunt) {
           }),
           new webpack.DefinePlugin({
             'process.env': {
-              'NODE_ENV': JSON.stringify('production')
+              NODE_ENV: JSON.stringify('production')
             }
           }),
           new webpack.optimize.AggressiveMergingPlugin(),
@@ -74,37 +72,38 @@ module.exports = function (grunt) {
         ]
       }
     },
-    "webpack-dev-server": {
+    'webpack-dev-server': {
       options: {
         webpack: webpackConfig,
-        publicPath: "/build/"
+        publicPath: '/build/'
       },
       dev: {
         hot: true,
         port: 3000,
+        https: true,
         webpack: {
-          devtool: "eval"
+          devtool: 'eval'
         }
       },
       design: {
         webpack: {
           entry: './support/design/index.js',
           output: {
-            path: path.join(__dirname, "build"),
+            path: path.join(__dirname, 'build'),
             filename: 'lock.design.js'
           },
-          devtool: "eval"
+          devtool: 'eval'
         }
       }
     },
-    "i18n": {
+    i18n: {
       build: {
         files: [
           {
             expand: true,
-            cwd: "lib/i18n",
-            src: "**/*.js",
-            dest: "build",
+            cwd: 'lib/i18n',
+            src: '**/*.js',
+            dest: 'build',
             ext: '.js'
           }
         ]
@@ -112,25 +111,25 @@ module.exports = function (grunt) {
     }
   });
 
-  grunt.loadNpmTasks("grunt-babel");
-  grunt.loadNpmTasks("grunt-webpack");
-  grunt.loadNpmTasks("grunt-contrib-clean");
-  grunt.loadNpmTasks("grunt-env");
-  grunt.loadNpmTasks("grunt-exec");
+  grunt.loadNpmTasks('grunt-babel');
+  grunt.loadNpmTasks('grunt-webpack');
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-env');
+  grunt.loadNpmTasks('grunt-exec');
 
-  grunt.registerTask("build", ["clean:build", "env:build", "webpack:build", "i18n:build"]);
-  grunt.registerTask("dist", ["clean:dist", "babel:dist"]);
-  grunt.registerTask("prepare_dev", ["clean:dev"]);
-  grunt.registerTask("dev", ["prepare_dev", "webpack-dev-server:dev"]);
-  grunt.registerTask("design", ["prepare_dev", "webpack-dev-server:design"]);
-  grunt.registerMultiTask("i18n", "Prepares i18n files to be hosted in CDN", function () {
+  grunt.registerTask('build', ['clean:build', 'env:build', 'webpack:build', 'i18n:build']);
+  grunt.registerTask('dist', ['clean:dist', 'babel:dist']);
+  grunt.registerTask('prepare_dev', ['clean:dev']);
+  grunt.registerTask('dev', ['prepare_dev', 'webpack-dev-server:dev']);
+  grunt.registerTask('design', ['prepare_dev', 'webpack-dev-server:design']);
+  grunt.registerMultiTask('i18n', 'Prepares i18n files to be hosted in CDN', function() {
     var languages = {};
     var Auth0 = {
-      registerLanguageDictionary: function (lang, dict) {
+      registerLanguageDictionary: function(lang, dict) {
         languages[lang] = dict;
       }
     };
-    this.files.forEach(function (file) {
+    this.files.forEach(function(file) {
       var filename = file.src[0];
       var lang = path.basename(filename, '.js');
       var dict = require('./' + filename).default || require('./' + filename);
