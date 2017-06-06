19c15e2f9189823029b09bb050048a002802ea29
midler <marcel.idler.extern@holidaycheck.com>
2017-06-06 10:59:35 +0200

    Scroll messages in all lock dialogs into view

--- src/ui/box/global_message.jsx
+++ src/ui/box/global_message.jsx
@@ -2,11 +2,14 @@ import PropTypes from 'prop-types';
 import React from 'react';
 
 export default class GlobalMessage extends React.Component {
+  componentDidMount() {
+      this.messageBox.scrollIntoView(true);
+  }
   render() {
     const { message, type } = this.props;
     const className = `auth0-global-message auth0-global-message-${type}`;
     return (
-      <div className={className}>
+      <div className={className} ref={(message) => { this.messageBox = message; }}>
         <span className="animated fadeInUp">{message}</span>
       </div>
     );
