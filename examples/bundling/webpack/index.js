be947bfbe6aad84ee7b9811e033084f209061e40
Luís Rudge <luis@luisrudge.net>
2017-10-11 18:42:10 -0300

    Upgrading webpack example (#1143)
    * Upgrading webpack example
    
    * Adding link to official react docs on how to build a prod version of react

--- examples/bundling/webpack/index.js
+++ examples/bundling/webpack/index.js
@@ -1,8 +1,31 @@
 import Auth0Lock from 'auth0-lock';
 
-const cid = "yKJO1ckwuY1X8gPEhTRfhJXyObfiLxih";
-const domain = "mdocs.auth0.com";
+const cid = 'BWDP9XS89CJq1w6Nzq7iFOHsTh6ChS2b';
+const domain = 'brucke.auth0.com';
+const options = {
+  oidcConformant: true,
+  allowShowPassword: true,
+  usernameStyle: 'email',
+  defaultDatabaseConnection: 'acme',
+  prefill: {
+    email: 'johnfoo@gmail.com'
+  }
+};
 
-const lock = new Auth0Lock(cid, domain);
+const lock = new Auth0Lock(cid, domain, options);
+
+lock.on('authenticated', function(authResult) {
+  console.log(authResult);
+
+  if (!authResult.accessToken) return;
+
+  lock.getUserInfo(authResult.accessToken, function(error, profile) {
+    console.log(error, profile);
+  });
+});
+
+lock.on('authorization_error', function(error) {
+  console.log('authorization_error', error);
+});
 
 lock.show();
