b97c20e8a35a281fb32f4946a78bdb1481bfadde
Luis Deschamps Rudge <luis@luisrudge.net>
2017-11-30 19:02:50 -0200

    Mapping access_denier error to invalid_user_password

--- src/__tests__/core/web_api/helper.test.js
+++ src/__tests__/core/web_api/helper.test.js
@@ -1,5 +1,5 @@
 import expect from 'expect.js';
-import { webAuthOverrides } from 'core/web_api/helper';
+import { webAuthOverrides, normalizeError } from 'core/web_api/helper';
 
 describe('webAuthOverrides', () => {
   it('should return overrides if any field is compatible with WebAuth', function() {
@@ -17,5 +17,20 @@ describe('webAuthOverrides', () => {
 
   it('should return null if no fields are compatible with WebAuth', function() {
     expect(webAuthOverrides({ backgroundColor: 'blue' })).to.not.be.ok();
+  });
+});
+describe('normalizeError', () => {
+  it('should map access_denied error to invalid_user_password', () => {
+    const error = {
+      error: 'access_denied',
+      description: 'foobar'
+    };
+    const expectedError = {
+      code: 'invalid_user_password',
+      error: 'invalid_user_password',
+      description: error.description
+    };
+    const actualError = normalizeError(error);
+    expect(actualError).to.be.eql(expectedError);
   });
 });
