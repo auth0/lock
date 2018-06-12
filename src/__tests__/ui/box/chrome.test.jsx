6875a7022a456abe3ba2fe414a30f2511442eff8
Luis Deschamps Rudge <luis@luisrudge.net>
2018-06-12 19:27:23 -0300

    test

---
+++ src/__tests__/ui/box/chrome.test.jsx
@@ -0,0 +1,45 @@
+const getChrome = () => {
+  return require('ui/box/chrome').default;
+};
+
+describe('Chrome', () => {
+  let oldConsole;
+  beforeEach(() => {
+    oldConsole = console;
+    global.console = { log: jest.fn() };
+  });
+  afterEach(() => {
+    global.console = oldConsole;
+  });
+  it('`debug` calls console.log when __DEV__', () => {
+    global.__DEV__ = true;
+    const Chrome = getChrome();
+    var c = new Chrome({
+      contentProps: {
+        model: {
+          toJS: () => 'jsmodel'
+        }
+      }
+    });
+
+    c.debug();
+    const { mock } = global.console.log;
+    expect(mock.calls.length).toBe(1);
+    expect(mock.calls[0][0]).toBe('jsmodel');
+  });
+  it('`debug` does not call console.log when !__DEV__', () => {
+    global.__DEV__ = false;
+    const Chrome = getChrome();
+    var c = new Chrome({
+      contentProps: {
+        model: {
+          toJS: () => 'jsmodel'
+        }
+      }
+    });
+
+    c.debug();
+    const { mock } = global.console.log;
+    expect(mock.calls.length).toBe(0);
+  });
+});
