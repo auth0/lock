7713fbca8d09c397024e8390c901a45e5d61deb5
Luís Rudge <luis@luisrudge.net>
2018-07-26 14:53:55 -0300

    Fix crash when showing lock for the second time with custom select input (#1448)
    fix https://github.com/auth0/lock/issues/1434

--- src/engine/classic.js
+++ src/engine/classic.js
@@ -14,7 +14,8 @@ import {
   hasInitialScreen,
   hasScreen,
   initDatabase,
-  overrideDatabaseOptions
+  overrideDatabaseOptions,
+  resolveAdditionalSignUpFields
 } from '../connection/database/index';
 import {
   isADEnabled,
@@ -152,6 +153,7 @@ class Classic {
 
   willShow(m, opts) {
     m = overrideDatabaseOptions(m, opts);
+    m = resolveAdditionalSignUpFields(m);
     if (isSuccess(m, 'client')) {
       m = validateAllowedConnections(m);
     }
