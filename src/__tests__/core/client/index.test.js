1491c482a7c59a5d3155199fe9ec58b7f94ee3ad
Luis Deschamps Rudge <luis@luisrudge.net>
2018-08-28 14:30:04 -0300

    Fix tests

--- src/__tests__/core/client/index.test.js
+++ src/__tests__/core/client/index.test.js
@@ -3,47 +3,41 @@ import { initClient } from '../../../core/client';
 
 describe('core/client/index', () => {
   describe('initClient', () => {
-    it('loads password policy correctly without a password_complexity_options option', () => {
-      const client = {
-        strategies: [
-          {
-            name: 'auth0',
-            connections: [
-              {
-                name: 'Username-Password-Authentication',
-                passwordPolicy: 'low'
-              }
-            ]
-          }
-        ]
-      };
-      const result = initClient(Immutable.fromJS({}), client).toJS();
-      expect(result.client.connections.database[0].passwordPolicy).toMatchObject({
-        length: {
-          minLength: 6
-        }
+    ['none', 'low', 'fair', 'good', 'excellent'].forEach(policy => {
+      it(`loads password policy '${policy}' correctly without a password_complexity_options option`, () => {
+        const client = {
+          strategies: [
+            {
+              name: 'auth0',
+              connections: [
+                {
+                  name: 'Username-Password-Authentication',
+                  passwordPolicy: policy
+                }
+              ]
+            }
+          ]
+        };
+        const result = initClient(Immutable.fromJS({}), client).toJS();
+        expect(result.client.connections.database[0].passwordPolicy.length).toMatchSnapshot();
       });
-    });
-    it('loads password policy correctly with a password_complexity_options option', () => {
-      const client = {
-        strategies: [
-          {
-            name: 'auth0',
-            connections: [
-              {
-                name: 'Username-Password-Authentication',
-                passwordPolicy: 'low',
-                password_complexity_options: { min_length: 4 }
-              }
-            ]
-          }
-        ]
-      };
-      const result = initClient(Immutable.fromJS({}), client).toJS();
-      expect(result.client.connections.database[0].passwordPolicy).toMatchObject({
-        length: {
-          minLength: 4
-        }
+      it(`loads password policy '${policy}' correctly with a password_complexity_options option`, () => {
+        const client = {
+          strategies: [
+            {
+              name: 'auth0',
+              connections: [
+                {
+                  name: 'Username-Password-Authentication',
+                  passwordPolicy: policy,
+                  password_complexity_options: { min_length: 4 }
+                }
+              ]
+            }
+          ]
+        };
+        const result = initClient(Immutable.fromJS({}), client).toJS();
+        expect(result.client.connections.database[0].passwordPolicy.length).toMatchSnapshot();
       });
     });
   });
