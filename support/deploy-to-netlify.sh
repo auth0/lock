5ff33ade1063072dd87f4526f74e7ac25f5d716e
Luis Deschamps Rudge <luis@luisrudge.net>
2018-12-14 20:29:19 -0200

    don't deploy to netlify

--- support/deploy-to-netlify.sh
+++ support/deploy-to-netlify.sh
@@ -5,6 +5,6 @@ cp ../build/lock.min.js netlify-deploy/lock.min.js
 cp ../build/lock.min.js.map netlify-deploy/lock.min.js.map
 cp ../build/en.js netlify-deploy/en.js
 npx -p postcss-cssnext -p postcss-cli -c "postcss index.css --use postcss-cssnext -o netlify-deploy/index.p.css"
-cd netlify-deploy
-netlify deploy --dir=. --prod
+# cd netlify-deploy
+# netlify deploy --dir=. --prod
 cd ..
