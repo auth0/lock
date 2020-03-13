544dd4cbb8467371f629aef3fa790a873f1d291a
Haqverdi Behbudov <haqverdib@gmail.com>
2020-03-13 15:29:30 +0400

    add Azerbaijan language
    add Azerbaijan language translations

---
+++ src/i18n/az.js
@@ -0,0 +1,134 @@
+// This file was automatically translated.
+// Feel free to submit a PR if you find a more accurate translation.
+
+export default {
+  error: {
+    forgotPassword: {
+      too_many_requests:
+        'Şifrəni dəyişdirməyə cəhdlərin sayında maksimal həddə çatmısınız. Xahiş edirik bir daha cəhd etmədən əvvəl gözləyin.',
+      'lock.fallback':
+        'Bağışlayın, şifrənizi dəyişdirmə sorğusu işlənərkən xəta baş verdi.',
+      enterprise_email:
+        'E-poçtunuzun sahəsi korporativ kimlik təminatçısının bir hissəsidir. Şifrənizi yenidən qurmaq üçün təhlükəsizlik administratorunuzla əlaqə saxlayın.'
+    },
+    login: {
+      blocked_user: 'İstifadəçi bloklandı.',
+      invalid_user_password: 'Məlumatlar yalnışdır.',
+      'lock.fallback': 'Üzr istəyirik, giriş zamanı səhv oldu.',
+      'lock.invalid_code': 'Yalnış kod.',
+        'lock.invalid_email_password': 'Yalnış e-poçt və ya şifrə.',
+      'lock.invalid_username_password': 'Yanlış istifadəçi adı və ya şifrə.',
+      'lock.network': 'Server ilə əlaqə qura bilmədik. Lütfən bağlantınızı yoxlayın və yenidən cəhd edin.',
+      'lock.popup_closed': 'Pop-up pəncərə bağlandı. Yenidən cəhd edin.',
+      'lock.unauthorized': 'Səlahiyyətlər əldə edilə bilmədi. Təkrar cəhd edin.',
+      'lock.mfa_registration_required':
+        'Çox faktorlu identifikasiya tələb olunur, lakin cihazınız qeydiyyatdan keçməyib. Lütfən, davam etməzdən əvvəl cihazınızı qeydiyyata alın.',
+        'lock.mfa_invalid_code': 'Yalnış kod. Xahiş edirik yenidən cəhd edin.',
+      password_change_required:
+        'Şifrənizi ilk dəfə daxil etdiyiniz və ya şifrənizin müddəti bitdiyinə görə yeniləməlisiniz.',
+      password_leaked:
+        'Parolunuz başqa bir veb saytında yayımlandığı üçün bu giriş cəhdi blok edildi. Kilidi açmaq üçün sizə e-poçt göndərdik.',
+      too_many_attempts: 'Çox sayda giriş cəhdi nəticəsində hesabınız bloklandı.',
+      session_missing:
+        'Doğrulama tələbi yerinə yetirilə bilmədi. Bütün açıq dialoqları bağladıqdan sonra yenidən cəhd edin',
+      'hrd.not_matching_email': ', Giriş üçün korporativ e-poçtdan istifadə edin.',
+      too_many_requests:
+        'Üzr istəyirik. Hazırda çox sayda müraciət var. Zəhmət olmasa səhifəni yenidən yükləyin və yenidən cəhd edin. Bu davam edərsə, daha sonra yenidən cəhd edin.'
+    },
+    passwordless: {
+      'bad.email': 'E-poçt düzgün deyil',
+      'bad.phone_number': 'Telefon nömrəsi düzgün deyil',
+      'lock.fallback': 'Üzr istəyirik, səhv oldu'
+    },
+    signUp: {
+      invalid_password: 'Şifrə yanlışdır.',
+      'lock.fallback': 'Üzr istəyirik, qeydiyyatdan keçmə zamanı səhv oldu.',
+      password_dictionary_error: 'Şifrə çox sadədir.',
+      password_no_user_info_error: 'Şifrə istifadəçi məlumatlarını ehtiva edir.',
+      password_strength_error: 'Şifrə çox sadədir.',
+      user_exists: 'Bu istifadəçi artıq mövcuddur.',
+      username_exists: 'Bu istifadəçi adı istifadə olunur.'
+    }
+  },
+  success: {
+    logIn: 'Giriş üçün təşəkkür edirik.',
+    forgotPassword: 'Şifrənizi yeniləmək üçün sizə e-poçt göndərdik.',
+    magicLink: 'Giriş üçün link göndərdik<br /> %s.',
+    signUp: 'Qeydiyyatdan keçdiyiniz üçün təşəkkür edirik.'
+  },
+  blankErrorHint: 'Boş ola bilməz',
+  codeInputPlaceholder: 'kodunuz',
+  databaseEnterpriseLoginInstructions: '',
+  databaseEnterpriseAlternativeLoginInstructions: 'və ya',
+  databaseSignUpInstructions: '',
+  databaseAlternativeSignUpInstructions: 'və ya',
+  emailInputPlaceholder: 'mail@example.com',
+  enterpriseLoginIntructions: 'Şirkətinizin məlumatları ilə daxil olun.',
+  enterpriseActiveLoginInstructions: 'Zəhmət olmasa %s ilə şirkətinizin kimlik nömrəsini daxil edin.',
+  failedLabel: 'Uğursuz!',
+  forgotPasswordAction: 'Şifrənizi xatırlamırsınız?',
+  forgotPasswordInstructions:
+    'Zəhmət olmasa, e-mail adresinizi daxil edin. Parolunuzu yeniləmək üçün sizə e-poçt göndərəcəyik.',
+  forgotPasswordSubmitLabel: 'Email göndər',
+  invalidErrorHint: 'Yalnış',
+  lastLoginInstructions: 'Sonuncu daxil olduğunuz',
+  loginAtLabel: '%s ilə daxil olun',
+  loginLabel: 'Daxil ol',
+  loginSubmitLabel: 'Daxil ol',
+  loginWithLabel: '%s ilə daxil olun',
+  notYourAccountAction: 'Sizin hesabınız deyil?',
+  passwordInputPlaceholder: 'şifrəniz',
+  passwordStrength: {
+    containsAtLeast:
+      'Ən az %d müxtəlif növ simvollardan istifadə edilməlidir. Alt-da istifadə oluna bilər %d müxtəlif növlər verilmişdir.',
+    identicalChars: 'Bir sətirdə %d dən çox eyni xarakter olmaz (nüm., "%s" icazə verilmir)',
+    nonEmpty: 'Boşluq olmayan parol tələb olunur',
+    numbers: 'Saylar (nüm. 0-9)',
+    lengthAtLeast: 'Ən az %d xarakter uzunluğunda',
+    lowerCase: 'Kiçik hərflər (a-z)',
+    shouldContain: 'İbarət olmalı:',
+    specialCharacters: 'Xüsusi simvollar (nüm. !@#$%^&*)',
+    upperCase: 'Böyük hərflər (A-Z)'
+  },
+  passwordlessEmailAlternativeInstructions:
+    'Əks halda, giriş üçün e-poçt adresinizi daxil edin<br/>ya da qeydiyyatdan keçin',
+  passwordlessEmailCodeInstructions: '%s ünvanına kod olan email göndərildi.',
+  passwordlessEmailInstructions:
+    'Giriş üçün e-mail adresinizi daxil edin<br/>və ya qeydiyyatdan keçin',
+  passwordlessSMSAlternativeInstructions:
+    'Əks halda, giriş üçün telefon nömrənizi daxil edin<br/>və ya qeydiyyatdan keçin',
+  passwordlessSMSCodeInstructions: 'Sizə SMS göndərdik<br/>%s.',
+  passwordlessSMSInstructions:
+    'Giriş üçün telefon nömrənizi daxil edin<br/>və ya qeydiyyatdan keçin',
+  phoneNumberInputPlaceholder: 'telefon nömrəniz',
+  resendCodeAction: 'Kod almadınız?',
+  resendLabel: 'Təkrar göndər',
+  resendingLabel: 'Təkrar göndərilir...',
+  retryLabel: 'Yenidən cəhd edin',
+  sentLabel: 'Göndərildi!',
+  signUpLabel: 'Qeydiyyatdan keç',
+  signUpSubmitLabel: 'Qeydiyyatdan keç',
+  signUpWithLabel: '%s ilə qeydiyyatdan keçin',
+  socialLoginInstructions: '',
+  socialSignUpInstructions: '',
+  ssoEnabled: 'Tək giriş (SSO) aktivləşdirildi',
+  submitLabel: 'Göndər',
+  unrecoverableError: 'Xəta baş verdi.<br />Zəhmət olmasa texniki dəstəklə əlaqə saxlayın.',
+  usernameFormatErrorHint:
+    '%d-%d hərf, nömrələr və aşağıdakı simvollardan istifadə edin: "_", ".", "+", "-"',
+  usernameInputPlaceholder: 'istifadəçi adınızı',
+  usernameOrEmailInputPlaceholder: 'istifadəçi adı/e-poçt',
+  title: 'Auth0',
+  welcome: 'Xoş gəldiniz %s!',
+  windowsAuthInstructions: 'İş şəbəkənizdən bağlandınız&hellip;',
+  windowsAuthLabel: 'Windows Kimlik Təsdiqi',
+  mfaInputPlaceholder: 'Kod',
+  mfaLoginTitle: '2 adımda təsdiq',
+  mfaLoginInstructions: 'Zəhmət olmasa mobil tətbiqetmənin yaratdığı təsdiq kodunu daxil edin.',
+  mfaSubmitLabel: 'Daxil ol',
+  mfaCodeErrorHint: '%d nömrələrdən istifadə edin',
+  forgotPasswordTitle: 'Şifrənizi sıfırlayın',
+  signUpTitle: 'Qeydiyyatdan Keç',
+  showPassword: 'Şifrəni göstər',
+  signUpTerms: 'Qeydiyyatdan keçərək xidmət və məxfilik siyasətimizlə razılaşırsınız.'
+};
