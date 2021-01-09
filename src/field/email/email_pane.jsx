66ddfe7a577e7697b6810432b045aa98aaa4aa0f
Saltuk Alakus <saltukalakus@gmail.com>
2021-01-10 00:42:49 +0300

    Remove the error message for HRD only mode for the email field. Fix the issue with the HRD domain message

--- src/field/email/email_pane.jsx
+++ src/field/email/email_pane.jsx
@@ -37,12 +37,14 @@ export default class EmailPane extends React.Component {
     // TODO: invalidErrorHint and blankErrorHint are deprecated.
     // They are kept for backwards compatibiliy in the code for the customers overwriting
     // them with languageDictionary. They can be removed in the next major release.
-    const invalidHint =
-      field.get('invalidHint') || value
-        ? i18n.str('invalidErrorHint') || i18n.str('invalidEmailErrorHint')
+    const errMessage = value ? i18n.str('invalidErrorHint') || i18n.str('invalidEmailErrorHint')
         : i18n.str('blankErrorHint') || i18n.str('blankEmailErrorHint');
+    const invalidHint = field.get('invalidHint') || errMessage;
 
-    const isValid = (!forceInvalidVisibility || valid) && !c.isFieldVisiblyInvalid(lock, 'email');
+
+    let isValid = (!forceInvalidVisibility || valid) && !c.isFieldVisiblyInvalid(lock, 'email');
+    // Hide the error message for the blank email in Enterprise HRD only mode when the password field is hidden.
+    isValid = (forceInvalidVisibility && value === '') ? true : isValid;
 
     return (
       <EmailInput
