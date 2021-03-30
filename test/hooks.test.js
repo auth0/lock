678112d844eb260453f58a772596dcbed45e8577
Steve Hobbs <steve.hobbs.mail@gmail.com>
2021-03-30 13:39:46 +0100

    Fill out more acceptance tests for signingUp hook

--- test/hooks.test.js
+++ test/hooks.test.js
@@ -6,20 +6,143 @@ describe('executing public hooks', () => {
   after(h.restoreWebApis);
 
   describe('when a hook has been defined', function() {
-    it('should run the hook on login', function(done) {
-      const options = {
-        rememberLastLogin: false,
-        hooks: {
-          loggingIn: function() {
-            done();
+    describe('during login', function() {
+      it('should run the loggingIn hook', function(done) {
+        const options = {
+          rememberLastLogin: false,
+          hooks: {
+            loggingIn: function() {
+              done();
+            }
           }
-        }
-      };
+        };
 
-      const lock = h.displayLock("test", options, () => {
-        h.fillEmailInput(lock, 'test@test.com');
-        h.fillPasswordInput(lock, 'anypass');
-        h.submit(lock);
+        const lock = h.displayLock('test', options, () => {
+          h.logInWithEmailAndPassword(lock);
+          expect(h.hasErrorMessage(lock)).not.to.be.ok();
+        });
+      });
+
+      it('should display the custom error on exception', function(done) {
+        const options = {
+          rememberLastLogin: false,
+          hooks: {
+            loggingIn: function() {
+              throw { code: 'hook_error', description: 'This is a custom error' };
+            }
+          }
+        };
+
+        const lock = h.displayLock('test', options, () => {
+          h.logInWithEmailAndPassword(lock);
+
+          h.waitUntilErrorExists(
+            lock,
+            () => {
+              expect(h.hasErrorMessage(lock, 'This is a custom error')).to.be.ok();
+              lock.hide();
+              done();
+            },
+            1000
+          );
+        });
+      });
+
+      it('should display the fallback error on exception', function(done) {
+        const options = {
+          rememberLastLogin: false,
+          hooks: {
+            loggingIn: function() {
+              throw 'Should not run this';
+            }
+          }
+        };
+
+        const lock = h.displayLock('test', options, () => {
+          h.logInWithEmailAndPassword(lock);
+
+          h.waitUntilErrorExists(
+            lock,
+            () => {
+              expect(h.hasErrorMessage(lock, en.error.login['lock.fallback'])).to.be.ok();
+              lock.hide();
+              done();
+            },
+            1000
+          );
+        });
+      });
+    });
+
+    describe('during signUp', function() {
+      it('should run the signingUp hook', function(done) {
+        const options = {
+          rememberLastLogin: false,
+          initialScreen: 'signUp',
+          hooks: {
+            signingUp: function() {
+              done();
+            }
+          }
+        };
+
+        const lock = h.displayLock('test', options, () => {
+          expect(h.isSignUpTabCurrent(lock)).to.be.ok();
+          h.signUpWithEmailAndPassword(lock);
+          expect(h.hasErrorMessage(lock)).not.to.be.ok();
+        });
+      });
+
+      it('should display the custom error on exception', function(done) {
+        const options = {
+          rememberLastLogin: false,
+          initialScreen: 'signUp',
+          hooks: {
+            signingUp: function() {
+              throw { code: 'hook_error', description: 'This is a custom error' };
+            }
+          }
+        };
+
+        const lock = h.displayLock('test', options, () => {
+          h.signUpWithEmailAndPassword(lock);
+
+          h.waitUntilErrorExists(
+            lock,
+            () => {
+              expect(h.hasErrorMessage(lock, 'This is a custom error')).to.be.ok();
+              lock.hide();
+              done();
+            },
+            2000
+          );
+        });
+      });
+
+      it('should display the fallback error on exception', function(done) {
+        const options = {
+          rememberLastLogin: false,
+          initialScreen: 'signUp',
+          hooks: {
+            signingUp: function() {
+              throw 'Should not run this';
+            }
+          }
+        };
+
+        const lock = h.displayLock('test', options, () => {
+          h.signUpWithEmailAndPassword(lock);
+
+          h.waitUntilErrorExists(
+            lock,
+            () => {
+              expect(h.hasErrorMessage(lock, en.error.signUp['lock.fallback'])).to.be.ok();
+              lock.hide();
+              done();
+            },
+            1000
+          );
+        });
       });
     });
 
@@ -34,58 +157,10 @@ describe('executing public hooks', () => {
       };
 
       const lock = h.displayLock('test', options, () => {
-        h.fillEmailInput(lock, 'test@test.com');
-        h.fillPasswordInput(lock, 'anypass');
-        h.submit(lock);
+        h.logInWithEmailAndPassword(lock);
         expect(h.hasErrorMessage(lock)).not.to.be.ok();
         done();
-      })
-    });
-
-    it('should display the fallback error on exception', function(done) {
-      const options = {
-        rememberLastLogin: false,
-        hooks: {
-          loggingIn: function() {
-            throw 'Should not run this';
-          }
-        }
-      };
-
-      const lock = h.displayLock('test', options, () => {
-        h.fillEmailInput(lock, 'test@test.com');
-        h.fillPasswordInput(lock, 'anypass');
-        h.submit(lock);
-        
-        h.waitUntilErrorExists(lock, () => {
-          console.log(h.hasErrorMessage(lock, en.error.login['lock.fallback']));
-          lock.hide();
-          done();
-        }, 1000)
-      })
-    });
-
-    it('should display the custom error on exception', function(done) {
-      const options = {
-        rememberLastLogin: false,
-        hooks: {
-          loggingIn: function() {
-            throw { code: 'hook_error', description: 'This is a custom error' };
-          }
-        }
-      };
-
-      const lock = h.displayLock('test', options, () => {
-        h.fillEmailInput(lock, 'test@test.com');
-        h.fillPasswordInput(lock, 'anypass');
-        h.submit(lock);
-        
-        h.waitUntilErrorExists(lock, () => {
-          console.log(h.hasErrorMessage(lock, 'This is a custom error'));
-          lock.hide();
-          done();
-        }, 1000)
-      })
+      });
     });
   });
 });
