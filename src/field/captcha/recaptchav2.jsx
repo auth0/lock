5059936ee97335a623613c5ea89eb7819eddc367
José F. Romaniello <jfromaniello@gmail.com>
2021-04-12 17:12:05 -0300

    fix ESD-12716: move css hack to render function to fix recaptcha on signup

--- src/field/captcha/recaptchav2.jsx
+++ src/field/captcha/recaptchav2.jsx
@@ -65,22 +65,6 @@ export class ReCAPTCHA extends React.Component {
         'error-callback': this.erroredHandler,
         sitekey: this.props.sitekey
       });
-      /*
-        This is a hack for the following conflicting css-rule:
-
-        @media screen and (max-width: 480px)
-        html.auth0-lock-html body > * {
-            display: none;
-        }
-      */
-      const fixInterval = setInterval(() => {
-        const iframe = document.querySelector(`iframe[title="recaptcha challenge"]`);
-        if (!iframe) {
-          return;
-        }
-        iframe.parentNode.parentNode.style.display = 'block';
-        clearInterval(fixInterval);
-      }, 300);
     });
   }
 
@@ -89,7 +73,26 @@ export class ReCAPTCHA extends React.Component {
   }
 
   render() {
-    // style={{ border: !this.props.isValid ? '1px solid #dd4b39' : ''}}
+    /*
+      This is a hack for the following conflicting css-rule:
+
+      @media screen and (max-width: 480px)
+      html.auth0-lock-html body > * {
+          display: none;
+      }
+    */
+    const fixInterval = setInterval(() => {
+      let iframes = Array.from(document.querySelectorAll(`iframe[title="recaptcha challenge"]`));
+      iframes = iframes.filter(iframe => iframe.parentNode.parentNode.style.display !== 'block');
+      if (iframes.length === 0) {
+        return;
+      }
+      iframes.forEach(iframe => {
+        iframe.parentNode.parentNode.style.display = 'block';
+      });
+      clearInterval(fixInterval);
+    }, 300);
+
     return (
       <div
         className={
