315eafd73121c120ac60b41f72029ce3adb6ca3c
erombouts <elco.rombouts@gmail.com>
2021-07-09 09:18:17 +0200

    SL - Updated Dutch translations
    - letters to karakters
    - sentence constructions
    - English abbreviations for 'for example' like e.g., i.e., replaced by the Dutch bv.
    - Some cases changed je to u (or uw). Most labels use the more formal u. In English both je and u are 'you'.
    - Other changes

--- src/i18n/nl.js
+++ src/i18n/nl.js
@@ -1,13 +1,13 @@
 // This file was automatically translated.
-// Feel free to submit a PR if you find a more accurate translation.
+// Feel free to submit a pull request (PR) if you find a more accurate translation.
 
 export default {
   error: {
     forgotPassword: {
       too_many_requests:
-        'U heeft de limiet van wachtwoord aanpassings pogingen bereikt. Wacht even voor dat u het nog een keer probeert.',
+        'U heeft de limiet van het aantal wachtwoord wijzigingspogingen bereikt. Wacht even voor dat u het nog een keer probeert.',
       'lock.fallback':
-        'Onze excuses, er is iets fout gegaan bij de de aanvraag voor een wachtwoord aanpassing.',
+        'Onze excuses, er is iets fout gegaan bij de de aanvraag voor een wachtwoord wijziging.',
       enterprise_email:
         'Het domein van uw e-mail maakt deel uit van een Enterprise-identiteitsprovider. Raadpleeg uw beveiligingsbeheerder om uw wachtwoord opnieuw in te stellen.'
     },
@@ -16,28 +16,28 @@ export default {
       invalid_user_password: 'Gebruiker of wachtwoord is niet correct.',
       'lock.fallback': 'Onze excuses, er is iets mis gegaan bij het aanmelden.',
       'lock.invalid_code': 'Verkeerde code.',
-      'lock.invalid_email_password': 'Verkeerde e-mailadres of wachtwoord.',
+      'lock.invalid_email_password': 'Verkeerd e-mailadres of wachtwoord.',
       'lock.invalid_username_password': 'Verkeerde gebruikersnaam of wachtwoord.',
       'lock.network':
-        'Server is niet bereikbaar. Controleer uw netwerk verbinding en probeer het nog eens.',
-      'lock.popup_closed': 'Popup venster gesloten. Probeer het nog een keer.',
+        'Server is niet bereikbaar. Controleer uw netwerkverbinding en probeer het nog een keer.',
+      'lock.popup_closed': 'Pop-up venster gesloten. Probeer het nog een keer.',
       'lock.unauthorized': 'Autorisatie geweigerd. Probeer het nog een keer.',
       password_change_required:
         'U moet uw wachtwoord aanpassen omdat dit de eerste keer is dat u inlogt of omdat uw wachtwoord is verlopen.',
       password_leaked:
-        'Deze login is geblokkeerd omdat uw wachtwoord is gelekt op een andere website. We hebben een e-mail verstuurd met instructies voor het deblokkeren.',
+        'Dit account is geblokkeerd omdat uw wachtwoord is gelekt op een andere website. We hebben u een e-mail gestuurd met instructies voor het deblokkeren.',
       too_many_attempts:
-        'Uw account is geblokkeerd als gevolg van herhaalde pogingen om in te loggen.',
+        'Uw account is geblokkeerd als gevolg van te veel herhaalde inlogpogingen.',
       'lock.mfa_registration_required':
         'Diverse verificatie is vereist, maar het apparaat is niet geregistreerd. Gelieve in te schrijven voordat verder gaat.',
       'lock.mfa_invalid_code': 'Verkeerde code. Probeer het opnieuw.',
       session_missing:
-        'Kan uw authenticatie verzoek niet voltooien. Probeer opnieuw na het sluiten van alle geopende vensters',
+        'Kan uw authenticatie verzoek niet voltooien. Probeer het opnieuw na het sluiten van alle geopende vensters',
       'hrd.not_matching_email': 'Gelieve gebruik te maken van uw zakelijke e-mail om in te loggen.',
       too_many_requests:
         'Het spijt ons. Er zijn op dit moment te veel aanvragen. Laad de pagina opnieuw en probeer het opnieuw. Als dit probleem aanhoudt, probeert u het later opnieuw.',
-      invalid_captcha: 'Los de uitdagingsvraag op om te verifiëren dat je geen robot bent.',
-      invalid_recaptcha: 'Selecteer het selectievakje om te verifiëren dat u geen robot bent.'
+      invalid_captcha: 'Los de vraag op om te verifiëren dat u geen robot bent.',
+      invalid_recaptcha: 'Selecteer het vakje om te verifiëren dat u geen robot bent.'
     },
     passwordless: {
       'bad.email': 'Het e-mailadres is ongeldig',
@@ -45,21 +45,21 @@ export default {
       'lock.fallback': 'Onze excuses, er is iets fout gegaan.'
     },
     signUp: {
-      invalid_password: 'Wachtwoord is ongeldig.',
-      'lock.fallback': 'Onze excuses, er is iets mis gegaan bij de poging om te registreren.',
-      password_dictionary_error: 'Wachtwoord is een te bekend woord.',
-      password_no_user_info_error: 'Wachtwoord is gebaseerd op gebruikers informatie.',
-      password_strength_error: 'Wachtwoord is niet sterk genoeg.',
-      user_exists: 'Gebruiker bestaat al.',
-      username_exists: 'Gebruikersnaam bestaat al.',
+      invalid_password: 'Het wachtwoord is ongeldig.',
+      'lock.fallback': 'Onze excuses, er is iets mis gegaan bij het registreren.',
+      password_dictionary_error: 'Het wachtwoord is een te bekend woord.',
+      password_no_user_info_error: 'Het wachtwoord is gebaseerd op gebruikers informatie.',
+      password_strength_error: 'Het wachtwoord is niet sterk genoeg.',
+      user_exists: 'De gebruiker bestaat al.',
+      username_exists: 'De gebruikersnaam bestaat al.',
       social_signup_needs_terms_acception:
-        'Ga akkoord met de onderstaande Servicevoorwaarden om door te gaan.'
+        'Ga akkoord met de onderstaande servicevoorwaarden om door te gaan.'
     }
   },
   success: {
     logIn: 'Dank u voor het inloggen.',
-    forgotPassword: 'Wij hebben een e-mail verstuurd voor het resetten van uw wachtwoord.',
-    magicLink: 'Wij hebben een link verstuurd, naar %s, om mee in te loggen.',
+    forgotPassword: 'Wij hebben u een e-mail gestuurd voor het resetten van uw wachtwoord.',
+    magicLink: 'Wij hebben een link gestuurd, naar %s, om mee in te loggen.',
     signUp: 'Bedankt voor uw aanmelding.'
   },
   blankErrorHint: '',
@@ -67,14 +67,14 @@ export default {
   blankEmailErrorHint: 'Mag niet leeg zijn',
   blankUsernameErrorHint: 'Mag niet leeg zijn',
   blankCaptchaErrorHint: 'Mag niet leeg zijn',
-  codeInputPlaceholder: 'uw code',
+  codeInputPlaceholder: 'Uw code',
   databaseEnterpriseLoginInstructions: '',
   databaseEnterpriseAlternativeLoginInstructions: 'of',
   databaseSignUpInstructions: '',
   databaseAlternativeSignUpInstructions: 'of',
-  emailInputPlaceholder: 'iets@example.com',
-  enterpriseLoginIntructions: 'Login met uw bedrijfs inloggegevens.',
-  enterpriseActiveLoginInstructions: 'Voer uw bedrijfs inloggegevens in op %s.',
+  emailInputPlaceholder: 'iemand@voorbeeld.nl',
+  enterpriseLoginIntructions: 'Login met de gegevens van uw bedrijf.',
+  enterpriseActiveLoginInstructions: 'Voer de logingegevens van uw bedrijf in op %s.',
   failedLabel: 'Gefaald!',
   forgotPasswordAction: 'Wachtwoord vergeten?',
   forgotPasswordInstructions:
@@ -90,16 +90,16 @@ export default {
   loginSubmitLabel: 'Inloggen',
   loginWithLabel: 'Inloggen met %s',
   notYourAccountAction: 'Niet uw gebruikersaccount?',
-  passwordInputPlaceholder: 'uw wachtwoord',
+  passwordInputPlaceholder: 'Uw wachtwoord',
   passwordStrength: {
-    containsAtLeast: 'Bevat minsten %d van de volgende %d type karacters:',
-    identicalChars: 'Niet meer dan %d identieke letters op een rij (e.g., "%s" is niet toegestaan)',
-    nonEmpty: 'Wachtwoord mag niet leeg zijn',
-    numbers: 'Cijfers (i.e. 0-9)',
-    lengthAtLeast: 'Minstens %d letters lang',
-    lowerCase: 'kleine letters (a-z)',
+    containsAtLeast: 'Bevat minstens %d van de volgende %d type karakters:',
+    identicalChars: 'Niet meer dan %d identieke karakters op een rij (bv. "%s" is niet toegestaan)',
+    nonEmpty: 'Het wachtwoord mag niet leeg zijn',
+    numbers: 'Cijfers (0-9)',
+    lengthAtLeast: 'Minstens %d karakters lang',
+    lowerCase: 'Kleine letters (a-z)',
     shouldContain: 'Moet bevatten:',
-    specialCharacters: 'Speciale letters (e.g. !@#$%^&*)',
+    specialCharacters: 'Speciale karakters (bv. !@#$%^&*)',
     upperCase: 'Hoofdletters (A-Z)'
   },
   passwordlessEmailAlternativeInstructions:
@@ -112,7 +112,7 @@ export default {
   passwordlessSMSCodeInstructions: 'Er is een SMS met de code verstuurd naar %s.',
   passwordlessSMSInstructions:
     'Voer uw telefoonnummer in om in te loggen <br/>of creëer een nieuw gebruikersaccount',
-  phoneNumberInputPlaceholder: 'uw telefoonnummer',
+  phoneNumberInputPlaceholder: 'Uw telefoonnummer',
   resendCodeAction: 'Code niet ontvangen?',
   resendLabel: 'Opnieuw verzenden',
   resendingLabel: 'wordt verzonden...',
