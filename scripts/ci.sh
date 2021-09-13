c9f2f0969bfa64df466bc6a48dc92c8b42452b77
Steve Hobbs <steve.hobbs@auth0.com>
2021-09-13 12:00:07 +0100

    Remove Bower publishing from Jenkins CI script (#2033)
    * Remove Bower publishing from Jenkins CI script
    
    * Remove npm_release function (no longer used)

--- scripts/ci.sh
+++ scripts/ci.sh
@@ -42,48 +42,6 @@ cdn_release()
   success "$NPM_NAME ($1) uploaded to cdn"
 }
 
-bower_release()
-{
-  # Check if tag exists
-  TAG_NAME="v$VERSION"
-  TAG_EXISTS=$(git tag -l "$TAG_NAME")
-
-  if [ ! -z "$TAG_EXISTS" ]; then
-    verbose "There is already a tag $TAG_EXISTS in git. Skipping git deploy."
-  else
-    verbose "Deploying $VERSION to git"
-
-    LAST_COMMIT=$(git log -1 --pretty=%B)
-    grep -v -e '^build$' -e '^build/$' .gitignore > /tmp/.gitignore
-    mv /tmp/.gitignore .gitignore
-    git add --force build/*
-    git commit -am "$TAG_NAME"
-    git tag "$TAG_NAME" -m "$LAST_COMMIT"
-    git push origin $TAG_NAME
-    success "$NPM_NAME version ready for bower"
-  fi
-}
-
-npm_release()
-{
-  verbose "Checking if version $1 of $NPM_NAME is already available in npm…"
-
-  NPM_EXISTS=$(npm info -s $NPM_NAME@$1 version)
-
-  if [ ! -z "$NPM_EXISTS" ] && [ "$NPM_EXISTS" == "$1" ]; then
-    verbose "There is already a version $NPM_EXISTS in npm. Skipping npm publish…"
-  else
-    if [ ! -z "$STABLE" ]; then
-      verbose "Deploying $1 to npm"
-      npm publish
-    else
-      verbose "Deploying $1 to npm with tag $NPM_TAG"
-      npm publish --tag "$NPM_TAG"
-    fi
-    success "$NPM_NAME uploaded to npm registry"
-  fi
-}
-
 # Test
 yarn test
 yarn test:e2e
@@ -94,10 +52,7 @@ rm -f build/*.js
 # Build & Release Webpack Bundle
 yarn dist build
 git checkout -b dist
-bower_release
-new_line
 cdn_release "$VERSION"
 new_line
-
 git checkout master
 git branch -D dist
