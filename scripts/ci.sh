24251e2e7b522a194ffc0cff8a71f596f2a20f73
Steve Hobbs <steve.hobbs.mail@gmail.com>
2021-09-13 11:45:55 +0100

    Remove npm_release function (no longer used)

--- scripts/ci.sh
+++ scripts/ci.sh
@@ -42,26 +42,6 @@ cdn_release()
   success "$NPM_NAME ($1) uploaded to cdn"
 }
 
-npm_release()
-{
-  verbose "Checking if version $1 of $NPM_NAME is already available in npm…"
-
-  NPM_EXISTS=$(npm info -s $NPM_NAME@$1 version)
-
-  if [ ! -z "$NPM_EXISTS" ] && [ "$NPM_EXISTS" == "$1" ]; then
-    verbose "There is already a version $NPM_EXISTS in npm. Skipping npm publish…"
-  else
-    if [ ! -z "$STABLE" ]; then
-      verbose "Deploying $1 to npm"
-      npm publish
-    else
-      verbose "Deploying $1 to npm with tag $NPM_TAG"
-      npm publish --tag "$NPM_TAG"
-    fi
-    success "$NPM_NAME uploaded to npm registry"
-  fi
-}
-
 # Test
 yarn test
 yarn test:e2e
