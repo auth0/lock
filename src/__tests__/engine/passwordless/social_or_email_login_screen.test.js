7514bb1a3b954c559bd6eeca4d7dda172d53132b
kaki1104 <kaki.so2011@gmail.com>
2022-07-21 02:05:49 +0900

    updated tests

--- src/__tests__/engine/passwordless/social_or_email_login_screen.test.js
+++ src/__tests__/engine/passwordless/social_or_email_login_screen.test.js
@@ -1,101 +1,55 @@
 import React from 'react';
-import { expectShallowComponent } from 'testUtils';
-import { mockComponent } from 'testUtils';
-// import SocialOrEmailLoginScreen from 'engine/passwordless/social_or_email_login_screen'
+import { mount } from 'enzyme';
+
+import { expectComponent, extractPropsFromWrapper, mockComponent } from 'testUtils';
+
+//there's a circular dependency with this module, so we need to mock it
+jest.mock('engine/classic');
 
 jest.mock('field/social/social_buttons_pane', () => mockComponent('social_buttons_pane'));
+// jest.mock('core/error_screen', () => mockComponent('error_screen'));
+// jest.mock('core/loading_screen', () => mockComponent('loading_screen'));
 jest.mock('field/email/email_pane', () => mockComponent('email_pane'));
 // jest.mock('field/captcha/captcha_pane', () => mockComponent('captcha_pane'));
 jest.mock('core/pane_separator', () => mockComponent('pane_separator'));
 jest.mock('connection/database/sign_up_terms', () => mockComponent('sign_up_terms'));
-// jest.mock('engine/passwordless/social_or_email_login_screen', () => mockComponent('social_or_email_login_screen'));
+jest.mock('connection/passwordless/index', () => ({
+  isEmail: jest.fn()
+}));
 
-const getScreen = () => {
-  const SocialOrEmailScreen = require('engine/passwordless/social_or_email_login_screen').default;
-  return new SocialOrEmailScreen();
-};
 
 const getComponent = () => {
-  // return null;
-  console.log('hiii' + getScreen());
-  console.log(getScreen().render());
-  return getScreen().render();
+  const SocialOrEmailScreen = require('engine/passwordless/social_or_email_login_screen').default;
+  const screen = new SocialOrEmailScreen();
+  return screen.render();
 };
 
-describe('social or email login screen', () => {
+describe('email passwordless', () => {
   beforeEach(() => {
     jest.resetModules();
-    jest.mock('core/index', () => ({
-      hasSomeConnections: () => true,
-      prefill: () => ({
-        toJS() {
-          return {
-            email: 'prefill@example.com',
-            phoneNumber: '12354'
-          };
-        }
-      })
-    }));
-    jest.mock('field/email', () => ({
-      setEmail: jest.fn(m => m)
-    }));
-    jest.mock('field/phone_number', () => ({
-      setPhoneNumber: jest.fn(m => m)
-    }));
-    console.log('hi');
-  });
 
+    jest.mock('connection/database/index', () => ({
+      hasScreen: () => false
+    }));
+
+    jest.mock('connection/database/actions', () => ({
+      cancelMFALogin: jest.fn(),
+      logIn: jest.fn()
+    }));
+
+    jest.mock('core/signed_in_confirmation', () => ({
+      renderSignedInConfirmation: jest.fn()
+    }));
+  });
   const defaultProps = {
     i18n: {
-      str: (...keys) => keys.join(','),
-      group: (...keys) => keys.join(','),
-      html: (...keys) => keys.join(',')
+      str: (...keys) => keys.join(',')
     },
     model: 'model'
   };
-
-  //   it('renders correctly', async () => {
-  //     expectShallowComponent(<SocialOrEmailLoginScreen {...defaultProps} />).toMatchSnapshot();
-  //   });
-
-  it('renders empty div by default', () => {
+  it('renders correctly', () => {
     const Component = getComponent();
 
     expectComponent(<Component {...defaultProps} />).toMatchSnapshot();
   });
-
-  //   it('renders a captcha', () => {
-  //     require('core/index').captcha.mockReturnValue({
-  //       get() {
-  //         return true;
-  //       }
-  //     });
-
-  //     expectShallowComponent(<SocialOrEmailLoginScreen {...defaultProps} />).toMatchSnapshot();
-  //   });
-
-  //   it('hides the captcha for SSO connections', () => {
-  //     require('core/index').captcha.mockReturnValue({
-  //       get() {
-  //         return true;
-  //       }
-  //     });
-
-  //     require('engine/classic').isSSOEnabled.mockReturnValue(true);
-
-  //     expectShallowComponent(<LoginPane {...defaultProps} />).toMatchSnapshot();
-  //   });
-
-  //   it('shows the captcha for SSO (ADFS) connections', () => {
-  //     require('core/index').captcha.mockReturnValue({
-  //       get() {
-  //         return true;
-  //       }
-  //     });
-
-  //     require('engine/classic').isSSOEnabled.mockReturnValue(true);
-  //     require('connection/enterprise').isHRDDomain.mockReturnValue(true);
-
-  //     expectShallowComponent(<LoginPane {...defaultProps} />).toMatchSnapshot();
-  //   });
 });
