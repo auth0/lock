300915bcbff26786e1e114d2d7d5d28ae6d3e34a
ankitatripathi.mp@gmail.com <ankitatripathi.mp@gmail.com>
2025-10-14 14:59:39 +0530

    Fixed lint issues

--- debug-babel-plugin.js
+++
@@ -1,9 +0,0 @@
-module.exports = function myDebugPlugin() {
-  return {
-    visitor: {
-      Program(path, state) {
-        console.log("Babel processing file:", state.filename);
-      }
-    }
-  };
-};
